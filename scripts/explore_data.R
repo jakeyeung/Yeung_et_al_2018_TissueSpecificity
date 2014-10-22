3cf338d696c331698fb5291c7a81c6171f3a9979
Jake Yeung <jakeyeung@gmail.com>
2014-10-22 14:20:01 +0200

    addf functions for loadings and periodogram

--- scripts/explore_data.R
+++ scripts/explore_data.R
@@ -6,6 +6,29 @@ library(ggplot2)
 library(reshape)
 library(wordcloud)  # for showing text without jumbling
 
+
+# Define my functions -----------------------------------------------------
+
+plot_loadings <- function(y, title="Plot title"){
+  # Given vector from PCA, plot vector and color by tissue.
+  # Maybe give fancy legend
+  plot(y, main=title, col=rep(1:12, each=24), type='o') 
+}
+
+plot_periodogram <- function(x, N, title="Plot title"){
+  # x is vector of data you want to check for rhythmicity
+  N <- length(x)
+  FF <- abs(fft(x) / sqrt(N)) ^ 2  # do my fft
+  P <- FF[1:(N / 2 + 1)]  # only need first (N/2) + 1 values of FFT result
+  f <- (0:(N / 2) / N)  # creates harmonic frequencies from 0 to 0.5 in steps of 1/12
+  plot(f, P, type='l', main=title)
+}
+
+
+
+# MAIN --------------------------------------------------------------------
+
+
 # define dirs
 data_dir <- "microarray_data"
 # fname <- "GSE54650_series_matrix.no_headers.txt"    # data straight from hogenesch
@@ -96,8 +119,57 @@ for (x_comp in 1:5) {
 
 # Loading plot ------------------------------------------------------------
 
-for (vector in 1:20){
+# plot first n loadings
+n <- 20
+for (vector in 1:n){
   plot(dat_pca$x[, vector], main=paste("Vector", vector), col=rep(1:12, each=24), type='o')  
 }
+
+
+
+# Which vector loadings have oscillating components? ----------------------
+
+N <- 288  # number of samples
+T <- 24  # 24 hours in a period
+# Make nicer rownames
+
+rownames(dat_pca$x) <- newnames
+
+# Fit PCA component of interest
+for (pca_vector in 1:30){
+  pca_component_string <- paste("PCA component:", pca_vector)
+  # Create response vector, which is loadings
+  
+  y <- dat_pca$x[, pca_vector]
+  
+  # Create time vector, ignore different tissues
+  # from t=0 to t=46 every 2 hours. Repeat for 12 tissues
+  # t <- 1:288
+  
+  spec.pgram(y, log="no", main=pca_component_string)
+  plot_loadings(y, title=paste("PCA component:", pca_vector))
+  
+  # t <- rep(seq(0, 46, 2), 12)
+  t <- seq(0, 2 * N - 1, 2)
+  omega <- (2 * pi) / (T)
+  fit <- lm(y ~ cos(omega * t) + sin(omega * t))
+  print("*********************************")
+  print(paste0(pca_component_string))
+  cat("\n")
+  print(anova(fit))
+  cat("\n")
+}
+
+pca_vector = 14
+y <- dat_pca$x[, pca_vector]
+FF <- abs(fft(y) / sqrt(N)) ^ 2
+P <- FF[1:(N / 2 + 1)]
+f <- (0:(N / 2) / N)
+plot(f, P, type='l', main=paste("PCA Component:", pca_vector))
+
+
+
+
+
 
 
