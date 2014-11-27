727dd6ba34e601b4027a753ceef529b97a9323ec
Jake Yeung <jakeyeung@gmail.com>
2014-11-27 14:35:21 +0100

    Get loess fit by quantiles.

--- scripts/noise_model.R
+++ scripts/noise_model.R
@@ -4,6 +4,7 @@
 # Plot variance versus mean for each tissue and each gene.
 
 library(ggplot2)
+library(plyr)
 
 # Functions ---------------------------------------------------------------
 
@@ -14,17 +15,20 @@ source(file.path(functions.dir, 'SampleNameHandler.R'))  # make sample names
 # define directories ------------------------------------------------------
 
 # define dirs
-data_dir <- "data"
+data.dir <- "data"
 fname.rna.seq <- "rna_seq_deseq_counts_colnames_fixed.txt"
-
+fname.array <- "array_exprs_colnames_fixed2.txt"
 
 # Load file ---------------------------------------------------------------
 
 # load data: rnaseq
-rna.seq.path <- file.path(data_dir, fname.rna.seq)
+rna.seq.path <- file.path(data.dir, fname.rna.seq)
 print(paste("Reading data from,", rna.seq.path, "May take a few a minutes."))
 rna.seq.exprs <- read.table(rna.seq.path, header=TRUE, sep='\t')
 print("Read data to memory.")
+
+array.path <- file.path(data.dir, fname.array)
+array.exprs <- read.table(array.path, header=TRUE, sep='\t')
 
 # Handle duplicate rownames: RNASEQ ------------------------------------
 
@@ -34,6 +38,15 @@ drop.cols <- c("gene")
 rna.seq.exprs <- rna.seq.exprs[, !(names(rna.seq.exprs) %in% drop.cols)]
 
 Peek(rna.seq.exprs)  # expect gene names as row names, tissues in columns
+
+# Handle duplicate rownames: MICROARRAY -------------------------------------
+
+# first column contained gene names
+rownames(array.exprs) <- make.names(array.exprs$gene, unique=TRUE)
+
+drop.cols <- c("gene")
+array.exprs <- array.exprs[, !(names(array.exprs) %in% drop.cols)]
+Peek(array.exprs)
 
 
 # Get column names --------------------------------------------------------
@@ -68,29 +81,101 @@ str(mean.var)
 mean.var.df <- data.frame(mean=as.vector(mean.var$mean), var=as.vector(mean.var$var))
 
 
-# ggplot2 scatter ---------------------------------------------------------
+# Bin and fit loess -------------------------------------------------------
 
-ggplot(mean.var.df, aes(x=mean, y=var)) +
-  geom_point(shape=46, alpha=0.5) + 
-  scale_y_log10() +
-  scale_x_log10()
+n.per.bin <- 1000
+bins <- factor(round_any(mean.var.df$mean, n.per.bin, floor))
+bins.order <- factor(floor(order(mean.var.df$mean) / n.per.bin))
+mean.var.df <- cbind(mean.var.df, bins)
+mean.var.df <- cbind(mean.var.df, bins.order)
 
-# # Plot variance against mean ----------------------------------------------
-# 
-# set.seed(0)
-# n.subset.rows <- ceiling(0.01 * nrow(rna.seq.exprs))
-# subset.rows <- sample(rownames(rna.seq.exprs), n.subset.rows)
-# x <- unlist(mean.var$mean[subset.rows, ])
-# y <- unlist(mean.var$var[subset.rows, ])
-# plot(x, y, log="xy", cex=0.1, pch='.')
+# Bin mean and bin variance: by order -------------------------------------
 
+# take average in each bin for variance and mean.
+# bin.mean <- with(mean.var.df, (tapply(mean, bins.order, mean)))
+# bin.var <- with(mean.var.df, (tapply(var, bins.order, mean)))
 
-# Fit loess ---------------------------------------------------------------
+prob <- 0.5
 
-row.subset <- sample(nrow(mean.var.df), 0.01 * nrow(mean.var.df))
-mean.var.df.subset <- mean.var.df[row.subset, ]
+bin.mean <- with(mean.var.df, (tapply(mean, bins.order, function(x){
+  mean.quantiles <- quantile(x, prob)
+  return(mean.quantiles)
+})))
+bin.var <- with(mean.var.df, (tapply(var, bins.order, function(x){
+  var.quantiles <- quantile(x, prob)
+  return(var.quantiles)
+})))
 
-fit.loess <- loess(var ~ mean, mean.var.df.subset)
+plot(bin.mean, bin.var, main=paste0(n.per.bin, " genes per bin. ", prob, " quantiles."))
+fit.loess <- loess(bin.var ~ bin.mean)
 summary(fit.loess)
-plot(mean.var.df.subset$mean, mean.var.df.subset$var, log="xy")
-plot(fit.loess$x, predict(fit.loess), log="xy")
+x.loess <- sort(fit.loess$x)
+y.loess <- predict(fit.loess, x.loess)
+lines(x.loess, y.loess, col='red', lwd=3)
+
+
+# Get mean exprs across samples -------------------------------------------
+
+# Create subset and common genes so each microarray point has corresponding rnaseq
+common.genes <- intersect(rownames(array.exprs), rownames(rna.seq.exprs))
+common.samples <- intersect(colnames(array.exprs), colnames(rna.seq.exprs))
+array.exprs.subset.common.g <- 2^(array.exprs[common.genes, common.samples)  # normal scale
+rna.seq.exprs.common.g <- rna.seq.exprs[common.genes, ]
+Peek(array.exprs.subset.common.g)
+Peek(rna.seq.exprs.common.g)
+
+# Take avg across samples
+array.avg.exprs <- apply(array.exprs.subset.common.g, 1, mean)
+rna.avg.exprs <- apply(rna.seq.exprs.common.g, 1, mean)
+
+
+# Plot array and rna avg --------------------------------------------------
+
+plot(array.avg.exprs, 
+     rna.avg.exprs,
+     pch=46, cex=2, log="xy")
+
+
+
+# Fit noise model ---------------------------------------------------------
+
+# model: R = a ( m - b ), b < min(m) in linear scale
+
+clockgenes <- c('Nr1d1','Dbp', 'Arntl', 'Npas2', 'Nr1d2', 
+                'Bhlhe41', 'Nfil3', 'Cdkn1a', 'Lonrf3', 
+                'Tef', 'Usp2', 'Wee1', 'Dtx4', 'Asb12')
+                # 'Elovl3', 'Clock', 'Per1', 'Per2', 'Per3', 'Cry2', 'Cry1')
+# clockgenes <- c(clockgenes, 'Defb48', 'Svs1', 'Svs2', 'Svs5', 'Defb20', 'Adam7', 'Lcn8', 'Rnase10', 'Teddm1')
+
+R.hat <- function(m, a, b) a * (m - b)
+for (gene in clockgenes){
+  R <- rna.seq.exprs.common.g[gene, ]
+  M <- array.exprs.subset.common.g[gene, ]
+  M.full <- 2^array.exprs[gene, ]
+  
+  a.init <- 1  # expect to be > 1
+  b.init <- 0.5 * min(M.full)  # background some fraction of min exprs
+  
+  a.min <- 2^-4
+  a.max <- 2^4
+  b.min <- 0  # background can't be negative
+  b.max <- min(M.full)  # background can't be larger than min exprs
+  
+  R.var <- predict(fit.loess, data.frame(mean=unlist(R)))
+  
+  S <- function(x) sum((R - R.hat(M, x[1], x[2])) ^ 2 / R.var)
+  
+  a.b <- optim(c(a.init, b.init), S, method="L-BFGS-B",
+               lower=c(a.min, b.min),
+               upper=c(a.max, b.max))
+  a.hat <- a.b$par[1]
+  b.hat <- a.b$par[2]
+  
+  # predict
+  R.predict <- R.hat(M.full, a.hat, b.hat)
+  plot(unlist(M), unlist(R), main=paste0("gene=", gene, 
+                                         " a=", signif(a.hat, 2), 
+                                         " b=", signif(b.hat, 2),
+                                         " min.predict=", signif(min(R.predict))))
+  lines(M.full, R.predict, lwd=3, col='red')
+}
