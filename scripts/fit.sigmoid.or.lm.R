e8b94f77dd10e5fbc3cef6b76719fba36d84d760
Jake Yeung <jakeyeung@gmail.com>
2014-12-01 20:10:07 +0100

    F.tests and does sigmoid versus linear.

--- scripts/fit.sigmoid.or.lm.R
+++ scripts/fit.sigmoid.or.lm.R
@@ -6,6 +6,14 @@ library(plyr)
 library(ggplot2)
 
 
+# Constants to change -----------------------------------------------------
+
+pval <- 1e-10  # for F-test
+diagnostics.clock.plot.out <- 'plots/diagnostics.clock.lm.sigmoid.fit.log2.probe.pdf'
+before.after.clock.plot.out <- 'plots/clockgenes.lm.sigmoid.fit.log2.probe.pdf'
+diagnostics.tissue.plot.out <- 'plots/diagnostics.tissue.lm.sigmoid.fit.log2.probe.pdf'
+before.after.tissue.plot.out <- 'plots/tissuegenes.lm.sigmoid.fit.log2.probe.pdf'
+
 # Functions ---------------------------------------------------------------
 
 # SIGMOID FUNCTIONS 
@@ -29,12 +37,13 @@ clockgenes <- c('Nr1d1','Dbp', 'Arntl', 'Npas2', 'Nr1d2',
                 'Tef', 'Usp2', 'Wee1', 'Dtx4', 'Asb12')
 clockgenes <- c(clockgenes, 'Elovl3', 'Clock', 'Per1', 'Per2', 'Per3', 'Cry2', 'Cry1')
 clockgenes <- c(clockgenes, 'Defb48', 'Svs1', 'Svs2', 'Svs5', 'Defb20', 'Adam7', 'Lcn8', 'Rnase10', 'Teddm1')
+clockgenes <- c(clockgenes, "Sry")
 
 
 # Tissue genes ------------------------------------------------------------
 
 tissuegenes <- c("Plbd1","Acacb","Hadh","Decr1","Acadl","Ech1","Acsl1","Cpt2","Acaa2")  # PCA 1 from explore_data.R
-tissuegenes <- c(tissue.genes, "Elovl1","Slc35b3","Fkbp15","Slc39a8","Sep15","Mospd2","Med11","Slco2a1","Arf6","Yipf6")  # PCA 2 from explore_data.R
+tissuegenes <- c(tissuegenes, "Elovl1","Slc35b3","Fkbp15","Slc39a8","Sep15","Mospd2","Med11","Slco2a1","Arf6","Yipf6")  # PCA 2 from explore_data.R
 
 
 # Functions ---------------------------------------------------------------
@@ -45,6 +54,14 @@ source(file.path(functions.dir, 'SampleNameHandler.R'))  # make sample names
 source(file.path(functions.dir, 'RegressionFunctions.R'))  # optim function
 source(file.path(functions.dir, 'GeneTissueCalculations.R'))
 source(file.path(functions.dir, 'PlotFunctions.R'))
+
+
+# Define dirs -------------------------------------------------------------
+
+# define dirs
+data.dir <- "data"
+fname.rna.seq <- "rna_seq_deseq_counts_colnames_fixed.txt"
+fname.array <- "array_exprs_colnames_fixed.txt"
 
 # Load file ---------------------------------------------------------------
 
@@ -94,59 +111,6 @@ Peek(array.exprs.subset.common.g)
 Peek(rna.seq.exprs.common.g)
 
 
-# Linear regression -------------------------------------------------------
-
-coeff.mat <- matrix(NA, nrow=length(common.genes), ncol=2,
-                    dimnames=list(common.genes, 
-                                  c("slope", "intercept")))
-for (gene in common.genes){
-  R <- unlist(rna.seq.exprs.common.g[gene, ])
-  M <- unlist(array.exprs.subset.common.g[gene, ])
-  fit <- lm(M ~ R)  # R on y-axis, M on x-axis
-  coeff.mat[gene, "intercept"] <- fit$coefficients[1]
-  coeff.mat[gene, "slope"] <- fit$coefficients[2]
-}
-
-coeff.mat[clockgenes, ]
-
-# Fit sigmoidal curve: general --------------------------------------------
-# http://en.wikipedia.org/wiki/Generalised_logistic_function
-# A: lower asymptote
-# K: upper asymptote
-# B: growth rate
-# v > 0: affects near which asymptote max growth occurs
-# Q: depends on the value Y(0)
-# M: time to max growth if Q = v
-
-# init vals
-A0 <- 4
-K0 <- 13
-B0 <- 2
-v0 <- 5
-Q0 <- 10
-M0 <- 7.5
-
-# test
-# gene <- "Elovl3"
-# R <- unlist(rna.seq.exprs.common.g[gene, ])
-# x <- unlist(array.exprs.subset.common.g[gene, ])
-
-plot(R, sigmoid(A0, K0, B0, v0, Q0, M0, R))
-
-gene.test <- c("Elovl3", "Npas2", "Arntl", "Per1")
-for (gene in gene.test){
-  R.exprs <- unlist(rna.seq.exprs.common.g[gene, ])
-  M.exprs <- unlist(array.exprs.subset.common.g[gene, ])
-  fit.sigmoid <- nls(M.exprs ~ A + ((K - A) / (1 + Q * exp(-B * (R.exprs - M))) ^ (1 / v)),
-                     start=list(A=A0, 
-                                K=K0,
-                                B=B0,
-                                v=v0,
-                                Q=Q0,
-                                M=M0))
-  print(fit.sigmoid)
-}
-
 # Fit sigmoidal curve: 4 params --------------------------------------------
 
 # init vals
@@ -159,7 +123,7 @@ Bmin <- 0
 Cmin <- 3
 Dmin <- 13
 Amax <- Dmin
-Bmax <- 6  # too high and the slope is super sensitive
+Bmax <- 5  # too high and the slope is super sensitive
 Cmax <- 100
 Dmax <- Cmax
 
@@ -176,7 +140,7 @@ Dmax <- Cmax
 fit.list <- vector(mode="list", length=length(common.genes))
 names(fit.list) <- common.genes
 
-for (gene in clockgenes){
+for (gene in c(clockgenes, tissuegenes)){
   R.exprs <- unlist(rna.seq.exprs.common.g[gene, ])
   M.exprs <- unlist(array.exprs.subset.common.g[gene, ])
   fits <- tryCatch({
@@ -215,22 +179,32 @@ for (gene in clockgenes){
 
 # F-test on sigmoid and linear fit ----------------------------------------
 
-
-# pdf('plots/diagnostics.lm.sigmoid.fit.log2.probe.pdf')
-for (gene in clockgenes){
+fit.select.list <- vector(mode="list", length=length(common.genes))
+names(fit.select.list) <- common.genes
+for (gene in c(clockgenes, tissuegenes)){
   # check if sigmoid fit was performed...
   fit.sigmoid <- fit.list[[gene]][["sigmoid"]]
   fit.lm <- fit.list[[gene]][["lm"]]
-  
   # select either sigmoid or lm based on f.test or by default (i.e. sigmoid is NA)
-  fit.select <- FTestSigmoidLinearModels(fit.lm, fit.sigmoid)
+  fit.select <- FTestSigmoidLinearModels(fit.lm, fit.sigmoid, pval=pval)
+  fit.select.list[[gene]] <- fit.select
+}
+
+
+# Plot clock genes: diagnostics -------------------------------------------
+
+
+pdf(diagnostics.clock.plot.out)
+par(mfrow = c(2,1))
+for (gene in clockgenes){
+  fit.select <- fit.select.list[[gene]]
   fit.used <- fit.select$fit.used  # either sigmoid or lm
   myfit <- fit.select$myfit
   
   # Get vector of predicted values
-  x <- unlist(rna.seq.exprs.common.g[gene, ])
-  y <- unlist(array.exprs.subset.common.g[gene, ])
-  x.predict <- seq(min(x), max(x), 0.1)
+  x <- GetFullR(rna.seq.exprs, common.samples)
+  y <- unlist(array.exprs[gene, ])
+  x.predict <- seq(min(x)*0.8, max(x)*1.2, 0.1)
   if (fit.used == "sigmoid"){
     y.hat <- sigmoid(coef(myfit), x.predict)
   } else if (fit.used == "lm"){
@@ -241,73 +215,100 @@ for (gene in clockgenes){
   
   # plot 
   params.str <- paste0(signif(as.vector(coef(myfit)), 2), collapse=",")
-  plot(x, y, main=paste0("Gene=", gene, " Params=", params.str))
+  symbols <- GetUnobsObsSymbol(all.samples=colnames(array.exprs), common.samples, unobs=8, obs=1)
+  sizes <- GetUnobsObsSymbol(all.samples=colnames(array.exprs), common.samples, unobs=0.25, obs=1)
+  plot(x, y, main=paste0("Gene=", gene, " Params=", params.str), pch=symbols, cex=sizes,
+       xlab="RNA-Seq DESeq-normalized counts (log2)",
+       ylab="Microarray log2")
   lines(x.predict, y.hat)
-  # plot(y, x, main=paste0("Gene=", gene, " Params=", params.str))
-  # lines(y.hat, x.predict)
+  plot(2^x - 1, 2^y, main=paste0("Gene=", gene, " Params=", params.str), pch=symbols, cex=sizes,
+       xlab="RNA-Seq DESeq-normalized counts",
+       ylab="Microarray normal scale")
+  lines(2^x.predict - 1, 2^y.hat)
 }
-# dev.off()
+dev.off()
 
 
-# Adjust my microarray ----------------------------------------------------
+# Adjust microarray -------------------------------------------------------
 
-array.adj <- matrix(NA, nrow=length(common.genes), ncol=ncol(array.exprs), 
-                    dimnames=list(common.genes, colnames(array.exprs)))
-for (gene in common.genes){
-  int <- coeff.mat[gene, "intercept"]
-  slope <- coeff.mat[gene, "slope"]
-  M.orig <- as.matrix(array.exprs[gene, ])
-  M.adj <- slope * M.orig + int
-  array.adj[gene, ] <- M.adj
+array.adj <- matrix(NA, nrow=nrow(array.exprs), ncol=ncol(array.exprs),
+                    dimnames=list(rownames(array.exprs), 
+                                  colnames(array.exprs)))
+for (gene in c(clockgenes, tissuegenes)){
+  fit.used <- fit.select.list[[gene]][["fit.used"]]
+  myfit <- fit.select.list[[gene]][["myfit"]]  # either sigmoid or lm
+  
+  array.exprs.gene <- as.matrix(array.exprs[gene, ])
+  
+  if (fit.used == "sigmoid"){
+    A <- coef(myfit)["A"]  # microarray cannot be less than A (bg)
+    # adjust all microarray values less than A to A.
+    array.exprs.gene[which(array.exprs.gene < A)] <- A
+    array.adj.gene <- sigmoid.inv(coef(myfit), array.exprs.gene)
+
+  } else if (fit.used == "lm"){
+    int <- coef(myfit)[[1]]
+    slope <- coef(myfit)[[2]]
+    if (is.na(slope)){
+      # if NA, assume it is infinity
+      slope <- Inf
+    }
+    array.adj.gene <- (array.exprs.gene - int) / slope
+  }
+  array.adj[gene, ] <- array.adj.gene
 }
-save(array.adj, file='array.adj.lm.fit.log2.by.probe.RData')
 
-# Plot clock genes --------------------------------------------------------
 
-N.TISSUES <- 12
-pdf('plots/clock.genes.log2.lin.fit.by.probe.pdf')
-par(mfrow=c(3,1))
+# Plot before and after clockgenes ----------------------------------------
+
+pdf(before.after.clock.plot.out)
 for (gene in clockgenes){
   PlotBeforeAfter(gene, array.exprs, array.adj, rna.seq.exprs, y.max=16)
 }
 dev.off()
 
 
-# Plot side by side: clock genes ------------------------------------------
+# Plot tissue genes: diagnostics -------------------------------------------
 
-pdf('plots/clock.genes.log2.lin.fit.by.probe.rna.seq.side.by.side.pdf')
-for (gene in clockgenes){
-  PlotAgainstRnaSeq(gene, rna.seq.exprs, array.adj, common.samples, y.max=16)  
+
+pdf(diagnostics.tissue.plot.out)
+par(mfrow = c(2,1))
+for (gene in tissuegenes){
+  fit.select <- fit.select.list[[gene]]
+  fit.used <- fit.select$fit.used  # either sigmoid or lm
+  myfit <- fit.select$myfit
+  
+  # Get vector of predicted values
+  x <- GetFullR(rna.seq.exprs, common.samples)
+  y <- unlist(array.exprs[gene, ])
+  x.predict <- seq(min(x)*0.8, max(x)*1.2, 0.1)
+  if (fit.used == "sigmoid"){
+    y.hat <- sigmoid(coef(myfit), x.predict)
+  } else if (fit.used == "lm"){
+    y.hat <- linear(coef(myfit), x.predict)
+  } else {
+    warning("Neither sigmoid nor lm")
+  }
+  
+  # plot 
+  params.str <- paste0(signif(as.vector(coef(myfit)), 2), collapse=",")
+  symbols <- GetUnobsObsSymbol(all.samples=colnames(array.exprs), common.samples, unobs=8, obs=1)
+  sizes <- GetUnobsObsSymbol(all.samples=colnames(array.exprs), common.samples, unobs=0.25, obs=1)
+  plot(x, y, main=paste0("Gene=", gene, " Params=", params.str), pch=symbols, cex=sizes,
+       xlab="RNA-Seq DESeq-normalized counts (log2)",
+       ylab="Microarray log2")
+  lines(x.predict, y.hat)
+  plot(2^x - 1, 2^y, main=paste0("Gene=", gene, " Params=", params.str), pch=symbols, cex=sizes,
+       xlab="RNA-Seq DESeq-normalized counts",
+       ylab="Microarray normal scale")
+  lines(2^x.predict - 1, 2^y.hat)
 }
 dev.off()
 
-# Plot tissue genes -------------------------------------------------------
+# Plot before and after tissuegenes ----------------------------------------
 
-
-pdf('plots/tissue.genes.log2.lin.fit.by.probe.pdf')
+pdf(before.after.tissue.plot.out)
 for (gene in tissuegenes){
   PlotBeforeAfter(gene, array.exprs, array.adj, rna.seq.exprs, y.max=16)
-}
-dev.off()
-
-
-# Plot side by side: tissue genes -----------------------------------------
-
-pdf('plots/tissue.genes.log2.lin.fit.by.probe.rna.seq.side.by.side.pdf')
-for (gene in tissuegenes){
-  PlotAgainstRnaSeq(gene, rna.seq.exprs, array.adj, common.samples, y.max=16)  
-}
-dev.off()
-
-# Visualize tissue genes ---------------------------------------------------
-
-# log(r) = a log(m) + b
-
-pdf('plots/diagnostics.tissue.lm.fit.log2.probe.pdf')
-for (gene in tissuegenes){
-  slope <- coeff.mat[gene, "slope"]
-  int <- coeff.mat[gene, "intercept"]
-  PlotDiagnostics(gene, array.exprs, rna.seq.exprs, 
-                  common.samples, slope, int)
 }
 dev.off()
