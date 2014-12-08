31b80b477d90c4e3e900e06cf6066288764aeaed
Jake Yeung <jakeyeung@gmail.com>
2014-12-08 17:35:16 +0100

    Use beta distribution for calculating pvalues from Rsquared. I commit now because I will change this to F-test.

--- scripts/find.oscillating.genes.R
+++ scripts/find.oscillating.genes.R
@@ -11,6 +11,16 @@ T <- 24  # hours
 omega <- 2 * pi / T
 
 
+# Define genes ------------------------------------------------------------
+
+clockgenes <- c("Nr1d1","Dbp","Arntl","Npas2","Nr1d2","Bhlhe41","Nfil3",
+                "Cdkn1a","Lonrf3","Tef","Usp2","Wee1","Dtx4","Asb12",
+                "Elovl3","Clock","Per1","Per2","Per3","Cry2","Cry1")
+tissuegenes <- c("Plbd1","Acacb","Hadh","Decr1","Acadl","Ech1","Acsl1","Cpt2","Acaa2")  # PCA 1 from explore_data.R
+tissuegenes <- c(tissuegenes, "Elovl1","Slc35b3","Fkbp15","Slc39a8","Sep15","Mospd2","Med11","Slco2a1","Arf6","Yipf6")  # PCA 2 from explore_data.R
+
+
+
 # Functions ---------------------------------------------------------------
 
 scripts.dir <- 'scripts'
@@ -25,6 +35,30 @@ oscillate <- function(params, omega, t){
   params[1] + params[2] * cos(omega * t) + params[3] * sin(omega * t)
 }
 
+# guess if simple or complex from fit parameters
+IsSimple <- function(fit, simple.df=3, complex.df=4){
+  # If fit has simple.df parameters, it is simple. Return TRUE
+  # if fit has complex.df parameter,s it is complex. Return FALSE
+  # 
+  # Args:
+  # fit from lm
+  # 
+  # Return:
+  # TRUE if simple
+  # FALSE if complex
+  # NA if neither, print warning.
+  n.params <- summary(fit)$df[[1]]
+  if (n.params == simple.df){
+    return(TRUE)
+  } else if (n.params == complex.df){
+    return(FALSE)
+  }
+    else 
+      warning(paste("Fit parameters neither has", simple.df, 
+                    "or", complex.df, "parameters. 
+                    It has", n.params, ". Defaulting to FALSE."))
+  return(FALSE)
+}
 
 # Define dirs -------------------------------------------------------------
 
@@ -34,6 +68,11 @@ normalized.array.fname <- "array.adj.0.07.txt"
 normalized.array.path <- file.path(data.dir, normalized.array.fname)
 rna.seq.fname <- "rna_seq_deseq_counts_colnames_fixed.txt"
 rna.seq.path <- file.path(data.dir, rna.seq.fname)
+
+
+# Define plot files -------------------------------------------------------
+
+fit.plot <- "plots/fourier.analysis.top.oscillators.pdf"
 
 # Load file ---------------------------------------------------------------
 
@@ -81,9 +120,6 @@ rna.seq.exprs <- log2(rna.seq.exprs + 1)
 
 # Merge data into long format ---------------------------------------------
 
-clockgenes <- c("Nr1d1","Dbp","Arntl","Npas2","Nr1d2","Bhlhe41","Nfil3",
-                "Cdkn1a","Lonrf3","Tef","Usp2","Wee1","Dtx4","Asb12",
-                "Elovl3","Clock","Per1","Per2","Per3","Cry2","Cry1")
 tissues <- GetTissues(colnames(normalized.array))
 times.array <- GetTimes(colnames(normalized.array))
 times.rnaseq <- GetTimes(colnames(rna.seq.exprs))
@@ -114,7 +150,7 @@ rm(long.array, long.rnaseq)
 fit.list <- vector(mode="list", length=length(filtered.genes))
 names(fit.list) <- filtered.genes
 
-for (gene in clockgenes){
+for (gene in c(clockgenes, tissuegenes)){
   dat.gene.tiss <- subset(dat[dat$gene == gene, ], tissue=="Adr")
   dat.gene.tiss <- dat.gene.tiss[order(dat.gene.tiss$time), ]
   # all negatives go to 0
@@ -144,14 +180,26 @@ for (gene in clockgenes){
 
 # Get top genes -----------------------------------------------------------
 
+pval.df <- data.frame(pvals=rep(NA, length(filtered.genes)), gene=filtered.genes, row.names=filtered.genes)
+
+for (gene in c(clockgenes, tissuegenes)){
+  pval <- fit.list[[gene]][["pval"]]
+  pval.df[gene, "pvals"] <- pval 
+}
+
+pval.df <- na.omit(pval.df)
 
 # Plot top genes ----------------------------------------------------------
 
-# top.genes <- 
-  
+top.genes <- rownames(pval.df[with(pval.df, order(pvals)), ])
+
+pdf(fit.plot)
 for (gene in top.genes){
   dat.gene.tiss <- subset(dat[dat$gene == gene, ], tissue=="Adr")
   dat.gene.tiss <- dat.gene.tiss[order(dat.gene.tiss$time), ]
+  
+  fit <- fit.list[[gene]][["fit"]]
+  
   # Get vectors for plotting
   y.combined <- dat.gene.tiss$exprs
   t.combined <- dat.gene.tiss$time
@@ -160,22 +208,27 @@ for (gene in top.genes){
   y.rnaseq <- dat.gene.tiss[dat.gene.tiss$experiment=="rnaseq", ]$exprs
   t.rnaseq <- dat.gene.tiss[dat.gene.tiss$experiment=="rnaseq", ]$time
   
-  t <- seq(18, 64, length.out = 100)
-  y.model.combined <- oscillate(params = coef(fit.simple), omega, t) 
-  # take 1st intercept, 3 and 4 are cos and sin coefficients
-  y.model.array <- oscillate(params = coef(fit.complex)[c(1, 3, 4)], omega, t)
-  y.model.rnaseq <- oscillate(params = coef(fit.complex)[c(2, 3, 4)], omega, t)
-  
-  # Plot vectors
+  # Plot GIVENS
   plot(t.combined, y.combined, main=gene)
   points(t.rnaseq, y.rnaseq, pch='*')
-  lines(t, y.model.combined, col='red')
-  lines(t, y.model.array, col='blue')
-  lines(t, y.model.rnaseq, col='orange')
+  
+  # BEGIN: get and plot y.models
+  t <- seq(18, 64, length.out = 100)  # tspan
+  # get fit parameters: first ask if simple or complex
+  if (IsSimple(fit)){
+    y.model.combined <- oscillate(params = coef(fit), omega, t)  
+    lines(t, y.model.combined, col='red')
+  } else {
+    # take 1st intercept, 3 and 4 are cos and sin coefficients
+    y.model.array <- oscillate(params = coef(fit)[c(1, 3, 4)], omega, t)
+    y.model.rnaseq <- oscillate(params = coef(fit)[c(2, 3, 4)], omega, t)    
+    lines(t, y.model.array, col='blue')
+    lines(t, y.model.rnaseq, col='orange')
+  }
   legend("topleft", c("combined.fit", "array.fit", "rnaseq.fit"),
          lty=c(1,1),
          lwd=c(2.5, 2.5),
          col=c("red", "blue", "orange"))  
 }
-
+dev.off()
                                    
