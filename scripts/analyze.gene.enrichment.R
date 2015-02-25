9823eef14282c3da93a98c3591caf2313648ad6f
Jake Yeung <jake.yeung@epfl.ch>
2015-02-25 12:08:12 +0100

    Can loop now through file names because it's better to be automated.

--- scripts/analyze.gene.enrichment.R
+++ scripts/analyze.gene.enrichment.R
@@ -1,93 +1,45 @@
-# source("http://bioconductor.org/biocLite.R")
-# biocLite("topGO")
-# source("http://bioconductor.org/biocLite.R")
-# biocLite("org.Mm.eg.db")
-
-library(topGO)
-library(org.Mm.eg.db)
-
+# analyze.gene.enrichment.R
+# 
 
 # Functions ---------------------------------------------------------------
+source("scripts/functions/AnalyzeGeneEnrichment.R")
 
-ConvertSym2Entrez <- function(gene.list, sym2entrez){
-  # convert to entrez, auto filtering here
-  gene.list.entrez <- sapply(gene.list, function(g){
-    entrez <- sym2entrez[[g]]
-    if (!is.null(entrez)){
-      return(entrez)
-    }
-  }, simplify = TRUE)
-  gene.list.entrez <- gene.list.entrez[!sapply(gene.list.entrez, is.null)]
-  return(unlist(gene.list.entrez))
+RemoveExtension <- function(fname){
+  # removes any .txt or .pdf, returns the fname without the extension
+  fname.split <- strsplit(fname, '\\.')
+  # remove last element from split string
+  fname.split <- fname.split[[1]][-length(fname.split[[1]])]
+  
+  # reassemble any potential dots in filename from before
+  fname.noext <- paste(fname.split, collapse = ".")
+  return(fname.noext)
 }
 
-# Create gene symbol to entrez object -------------------------------------
+# Loop through file names and get gene enrichment -------------------------
 
-sym2entrez <- as.list(org.Mm.egALIAS2EG)
-sym2entrez <- sym2entrez[!is.na(sym2entrez)]
+fnames <- read.table("plots/nconds/7_conds_filtered/files.txt", )  # full file paths
+fnames <- as.character(unlist(fnames))  # loop-able
 
+genes.bg <- read.table("plots/nconds/7_conds_filtered/filtered_genes.txt")
+genes.bg <- as.character(unlist(genes.bg))
 
-# Create entrez to GO ID object -------------------------------------------
+# precalculation of these objs makes things go faster
+sym2entrez <- CreateSym2Entrez()
+entrez2GO <- CreateEntrez2GO()
 
-entrez2GO.obj <- org.Mm.egGO
-mapped.genes <- mappedkeys(entrez2GO.obj)
-entrez2GO.full <- as.list(entrez2GO.obj[mapped.genes])
-# collapse sublist of GO-IDs to vector
-entrez2GO <- lapply(entrez2GO.full, function(x){
-  return(names(x))  # lapply is nice
-})
-
-
-# geneID2GO<- readMappings(file = system.file("examples/geneid2go.map", package = "topGO"))
-
-
-
-# Get GOData --------------------------------------------------------------
-
-
-top.genes <- read.table("plots/nconds/7_conds_filtered/7_conds_filtered4.txt")
-top.genes <- as.character(unlist(top.genes))  # to get correct naming after conversion
-
-all.genes <- rownames(dat.with.fit.filtered)  # from run_nconds.R
-
-genes.entrez <- ConvertSym2Entrez(all.genes, sym2entrez)
-top.genes <- ConvertSym2Entrez(top.genes, sym2entrez)
-
-sel.genes <- factor(as.integer(genes.entrez %in% top.genes))
-names(sel.genes) <- genes.entrez
-
-GOdata <- new("topGOdata",
-              description = "Test",
-              ontology = "BP",
-              allGenes = sel.genes,
-              nodeSize = 5,
-              annot = annFUN.gene2GO,
-              gene2GO = entrez2GO)
-
-
-# Run enrichment ----------------------------------------------------------
-
-result.fisher <- runTest(GOdata, algorithm = "classic", statistic = "fisher")
-n.tests <- length(score(result.fisher))
-
-# Look at results ---------------------------------------------------------
-
-all.res <- GenTable(GOdata, 
-                    classicFisher = result.fisher,
-                    # classicKS = result.KS,
-                    # elimKS = result.KS.elim,
-                    orderBy = "classicFisher",
-                    ranksOf = "classicFisher",
-                    topNodes = n.tests)
-# adjust pvalues
-all.res$FDRadj <- p.adjust(all.res$classicFisher, method = "BH")
-
-
-# Filter table by pvalue --------------------------------------------------
-
-all.res <- all.res[all.res$FDRadj <= 0.05, ]
-
-# Write to file -----------------------------------------------------------
-
-write.table(all.res, file = "plots/nconds/7_conds_filtered/TOPGo_model4.txt", quote = FALSE, sep = "\t", row.names = FALSE, col.names = TRUE)
-
+for (fname in fnames){
+  genes.hit <- read.table(fname)
+  genes.hit <- as.character(unlist(genes.hit))
+  # Run ontologies for MF, BP and CC
+  for (onto in c("BP", "MF", "CC")){
+    fname.noext <- RemoveExtension(fname)
+    fname.out <- paste0(fname.noext, '_', onto)
+    res <- AnalyzeGeneEnrichment(genes.bg, genes.hit, 
+                                 sym2entrez, entrez2GO, 
+                                 convert.sym.to.entrez = TRUE, 
+                                 which.ontology = onto, 
+                                 write.path = fname.out, 
+                                 node.size = 5, 
+                                 FDR.cutoff = 0.05)
+  }
+}
