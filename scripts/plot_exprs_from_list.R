aa66c1db08b89207eb98515171ffb3a1280e701e
Jake Yeung <jake.yeung@epfl.ch>
2015-02-26 10:51:34 +0100

    Plots expression from a hard coded list.

--- scripts/plot_exprs_from_list.R
+++ scripts/plot_exprs_from_list.R
@@ -8,11 +8,17 @@ source(file.path(scripts.dir, funcs.dir, "PlotGeneAcrossTissues.R"))
 
 dat <- LoadArrayRnaSeq()
 
-fname <- "/home/yeung/projects/tissue-specificity/plots/nconds/7_conds_filtered/7_conds_filtered2.txt"
+fname <- "/home/yeung/projects/tissue-specificity/plots/nconds/7_conds_filtered/7_conds_filtered6.txt"
 jgenes <- ReadListToVector(fname)
 
 dat.sub <- subset(dat, gene %in% jgenes)
 
+count <- 1
 for (jgene in jgenes){
-  PlotGeneAcrossTissues(subset(dat.sub, gene == jgene), jtitle = jgene)
+  print(jgene)
+  print(PlotGeneAcrossTissues(subset(dat.sub, gene == jgene), jtitle = jgene))
+  count <- count + 1
+  if (count > 20){
+    break
+  }
 }
