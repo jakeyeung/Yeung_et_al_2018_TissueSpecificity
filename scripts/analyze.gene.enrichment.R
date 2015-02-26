c311fc7b095b8890f42bfd76651f594838e4887e
Jake Yeung <jake.yeung@epfl.ch>
2015-02-26 08:52:34 +0100

    Use functions ReadListToVector and FixGeneName to make it easier to read and capture more gene names.

--- scripts/analyze.gene.enrichment.R
+++ scripts/analyze.gene.enrichment.R
@@ -1,8 +1,11 @@
 # analyze.gene.enrichment.R
 # 
+setwd("/home/yeung/projects/tissue-specificity")
 
 # Functions ---------------------------------------------------------------
 source("scripts/functions/AnalyzeGeneEnrichment.R")
+source("scripts/functions/ReadListToVector.R")
+source("scripts/functions/FixGeneName.R")
 
 RemoveExtension <- function(fname){
   # removes any .txt or .pdf, returns the fname without the extension
@@ -17,11 +20,15 @@ RemoveExtension <- function(fname){
 
 # Loop through file names and get gene enrichment -------------------------
 
-fnames <- read.table("plots/nconds/7_conds_filtered_05_amp/files.txt", )  # full file paths
-fnames <- as.character(unlist(fnames))  # loop-able
+# fnames <- read.table("plots/nconds/7_conds_filtered_05_amp/files.txt")  # full file paths
+# fnames <- as.character(unlist(fnames))  # loop-able
+fnames <- ReadListToVector("plots/nconds/7_conds_filtered_05_amp/files.txt")
+fnames <- unlist(sapply(fnames, FixGeneName))
 
-genes.bg <- read.table("plots/nconds/7_conds_filtered_05_amp/filtered_genes.txt")
-genes.bg <- as.character(unlist(genes.bg))
+# genes.bg <- read.table("plots/nconds/7_conds_filtered_05_amp/filtered_genes.txt")
+# genes.bg <- as.character(unlist(genes.bg))
+genes.bg <- ReadListToVector("plots/nconds/7_conds_filtered_05_amp/filtered_genes.txt")
+genes.bg <- unlist(sapply(genes.bg, FixGeneName))
 
 # precalculation of these objs makes things go faster
 sym2entrez <- CreateSym2Entrez()
@@ -30,13 +37,13 @@ entrez2GO <- CreateEntrez2GO()
 for (fname in fnames){
   genes.hit <- read.table(fname)
   genes.hit <- as.character(unlist(genes.hit))
-  if (length(genes.hit) < 20){
+  if (length(genes.hit) < 5){
     next
   }
   # Run ontologies for MF, BP and CC
   for (onto in c("BP", "MF", "CC")){
     fname.noext <- RemoveExtension(fname)
-    fname.out <- paste0(fname.noext, '_', onto)
+    fname.out <- paste0(fname.noext, '_', onto, ".GOtop")
     if (file.exists(fname.out)){
       next
     }
