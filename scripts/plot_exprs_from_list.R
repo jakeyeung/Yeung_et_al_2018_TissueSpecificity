41cfd77b21ecb5d5cd0c67f14729eae973093ac9
Jake Yeung <jake.yeung@epfl.ch>
2015-02-27 18:49:10 +0100

    Add library and setwd so I can run my file from iterm

--- scripts/plot_exprs_from_list.R
+++ scripts/plot_exprs_from_list.R
@@ -1,24 +1,32 @@
 # plot_exprs_from_list.R
 # February 25 2015
+library(ggplot2)
+setwd("/home/yeung/projects/tissue-specificity")
 
 scripts.dir <- "scripts"
 funcs.dir <- "functions"
 source(file.path(scripts.dir, funcs.dir, "LoadArrayRnaSeq.R"))
 source(file.path(scripts.dir, funcs.dir, "PlotGeneAcrossTissues.R"))
+source(file.path(scripts.dir, funcs.dir, "RemoveExtension.R"))
+source(file.path(scripts.dir, funcs.dir, "ReadListToVector.R"))
 
 dat <- LoadArrayRnaSeq()
 
-fname <- "/home/yeung/projects/tissue-specificity/plots/nconds/7_conds_filtered/7_conds_filtered6.txt"
-jgenes <- ReadListToVector(fname)
+fnames <- ReadListToVector("plots/nconds/7_conds_filtered_02_bicw/files.txt")
 
-dat.sub <- subset(dat, gene %in% jgenes)
+plot.dir <- "plots/nconds/7_conds_filtered_02_bicw/exprs_plots"
 
-count <- 1
-for (jgene in jgenes){
-  print(jgene)
-  print(PlotGeneAcrossTissues(subset(dat.sub, gene == jgene), jtitle = jgene))
-  count <- count + 1
-  if (count > 20){
-    break
-  }
+for (fname in fnames){
+	jgenes <- ReadListToVector(fname)
+
+	out.fname <- RemoveExtension(basename(fname))
+
+	dat.sub <- subset(dat, gene %in% jgenes)
+	
+	pdf(file.path(plot.dir, paste0(out.fname, ".pdf")))
+	for (jgene in jgenes){
+	  print(jgene)
+	  print(PlotGeneAcrossTissues(subset(dat.sub, gene == jgene), jtitle = jgene))
+	}
+	dev.off()
 }
