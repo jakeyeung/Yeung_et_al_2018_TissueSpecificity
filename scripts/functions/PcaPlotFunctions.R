b488d3217715d51bf4c1f86e7dd34adae2bde8e3
Jake Yeung <jake.yeung@epfl.ch>
2015-03-09 14:22:28 +0100

    Include plot.colors as a graph option.

--- scripts/functions/PcaPlotFunctions.R
+++ scripts/functions/PcaPlotFunctions.R
@@ -3,10 +3,13 @@
 # Periodogram plotting functions
 # Nov 5 2014
 # 
-PlotLoadings <- function(Loadings, title="Plot title", cex = 1) {
+PlotLoadings <- function(Loadings, title="Plot title", plot.colors, cex = 1) {
   # Given vector from PCA, plot vector and color by tissue.
   # Maybe give fancy legend
-  plot(Loadings, main=title, col=rep(1:12, each=24), type='o',
+  if (missing(plot.colors)){
+    plot.colors <- rep(1:12, each=24)
+  }
+  plot(Loadings, main=title, col=plot.colors, type='o',
        cex.axis = cex,
        cex.main = cex,
        cex.lab = cex)
