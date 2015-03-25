737c6578c4e9a416d7cb2a9e68287e2eddb83a06
Jake Yeung <jake.yeung@epfl.ch>
2015-03-25 18:09:55 +0100

    Pipeline-style

--- scripts/run_elastic_net.genomewide.R
+++ scripts/run_elastic_net.genomewide.R
@@ -10,25 +10,34 @@ source("scripts/functions/LoadAndHandleData.R")
 source("scripts/functions/RunElasticNet.R")
 
 args <- commandArgs(trailingOnly = TRUE)
-alpha = as.numeric(args[1])
+site_counts.fpath <- args[1]
+output.dir <- args[2]
+alpha <- as.numeric(args[3])
 
-x.uncorrected <- read.table(file = "site_count_matrices/sitecounts_matrix_EPD", row.names = 1, header = TRUE)
-x <- read.table(file = "site_count_matrices/corrected_sitecounts_EPD", row.names = 1, header = TRUE)
-conds <- c('Adr', 'Aorta', 'BFAT', 'Kidney', 'Liver', 'Lung', 'Mus')
+# site_counts.dir <- "site_count_matrices/GLM/SwissRegulon/N.MLE"
+# output.dir <- "plots/elastic_net/genome_wide/GLM/SwissRegulon/N.MLE"
+# alpha <- 0.05
+# x <- read.table(file = file.path(site_counts.dir, "sitecount_matrix"), header = TRUE)
+x <- read.table(file = site_counts.fpath, header = TRUE)
+x.rownames <- make.names(x[, 1], unique = TRUE)
+rownames(x) <- x.rownames
+x$Gene.ID <- NULL
+
 conds <- c('Liver')
 for (cond in conds){
   y <- read.table(paste0("y_input_elastic_net/genome_wide//genome_wide.", cond, ".elasticinput"))
-#   y <- read.table(paste0("y_input_elastic_net/7_conds_filtered26.", cond, ".elasticinput"))
-#   x <- read.table("site_count_matrices/7_conds_filtered26.sitecounts")
-  
+  head(rownames(x))
+  head(rownames(y))
   common.genes <- intersect(rownames(y), rownames(x))
-  
+  print(paste0(length(common.genes), " genes in common."))
   y <- as.matrix(y[common.genes, ])
   x <- as.matrix(x[common.genes, ])
   
   # Run elastic net ---------------------------------------------------------
   print(cond)
   start.time <- Sys.time()
-  RunElasticNet(y, x, paste0("plots/elastic_net/genome_wide/corrected_sitecounts/corrected_sitecounts_", cond), standardize = TRUE, alpha = alpha)
+  RunElasticNet(y, x, file.path(output.dir, paste0("corrected_sitecounts_", cond)), standardize = TRUE, alpha = alpha)
   print(Sys.time() - start.time)
 }
+
+print(warnings())
