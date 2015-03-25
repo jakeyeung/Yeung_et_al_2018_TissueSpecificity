ed39a6c879fe78f340704dc298982510da425043
Jake Yeung <jake.yeung@epfl.ch>
2015-03-25 18:16:38 +0100

    add source

--- scripts/MemoryManagement.R
+++ scripts/MemoryManagement.R
@@ -1,4 +1,5 @@
 # improved list of objects
+# thanks http://stackoverflow.com/questions/1358003/tricks-to-manage-the-available-memory-in-an-r-session
 .ls.objects <- function (pos = 1, pattern, order.by,
                          decreasing=FALSE, head=FALSE, n=5) {
   napply <- function(names, fn) sapply(names, function(x)
