a3b23ec1b08a70def2a91953886ee3865e67cc95
Jake Yeung <jake.yeung@epfl.ch>
2015-03-27 00:03:02 +0100

    dhs functions on its own

--- scripts/dhs_analyze_homerpeaks.R
+++ scripts/dhs_analyze_homerpeaks.R
@@ -5,50 +5,9 @@ library(ggplot2)
 
 # Functions ---------------------------------------------------------------
 
-GetPeakwidth <- function(col1){
-  # Given column 1 row 1 of autocorrelation.txt from homer,
-  # get Peakwidth
-  peakwidth <- strsplit(col1, split = '\\.\\.')[[1]][[4]] 
-  peakwidth <- as.numeric(peakwidth)
-  return(peakwidth)
-}
-
-GetFragLength <- function(col1){
-  # given col1 row1 get fraglength from autocorrelation.txt
-  fraglength <- as.numeric(strsplit(col1, split = '\\.\\.')[[1]][[2]])
-  return(fraglength)
-}
-
-Cnames <- function(){
-  # col2 is distance
-  # col2 and col3 just + and - strand
-  return(c("distance", "pos", "neg"))
-}
-
-Cnames.long <- function(){
-  return(c("distance", "n.obs", "strand", "peakwidth", "fraglength", "samp"))
-}
-
-ReadAutocorrelation <- function(f.path, samp.name){
-  # Read Autocorrelation.txt from homer output
-  samp.ac <- read.table(f.path, sep='\t', header=TRUE)
-  peakwidth <- GetPeakwidth(colnames(samp.ac)[1])
-  fraglength <- GetFragLength(colnames(samp.ac)[1])
-  colnames(samp.ac) <- Cnames()  # makes calling vectors easier
-  # column 1 header contains peakwidth info
-  samp.ac.long <- data.frame(distance = rep(samp.ac$distance, 2),
-                             n.obs = c(samp.ac$pos, samp.ac$neg),
-                             strand = c(rep("+", length(samp.ac$pos)), rep("-", length(samp.ac$neg))),
-                             peakwidth = rep(peakwidth, nrow(samp.ac) * 2),
-                             fraglength = rep(fraglength, nrow(samp.ac) * 2),
-                             samp = rep(samp.name, nrow(samp.ac) * 2))
-  cnames <- Cnames.long()
-  colnames(samp.ac.long) <- cnames
-  return(samp.ac.long)
-}
+source("scripts/functions/DhsFunctions.R")
 
 # Main --------------------------------------------------------------------
-
 
 dat.dir <- "/home/yeung/projects/tissue-specificity/data/homerpeaks"
 dir.vec <- list.files(dat.dir)
@@ -61,7 +20,6 @@ cnames <- Cnames.long()
 dat.merged <- data.frame(matrix(nrow = 0, ncol = length(cnames)))
 colnames(dat.merged) <- cnames
 
-
 # Loop through files, create merged data in long format -------------------
 
 
@@ -69,16 +27,15 @@ fname <- "tagAutocorrelation.txt"
 for (samp in samp.paths){
   samp.base <- basename(samp)
   f.path <- file.path(samp, fname)
-  dat.ac <- ReadAutocorrelation(f.path, samp.base)
-  dat.merged <- rbind(dat.merged, dat.ac)
+  dat.temp <- ReadAutocorrelation(f.path, samp.base)
+  dat.merged <- rbind(dat.merged, dat.temp)
 }
-
 
 # Create dat labels for plotting ------------------------------------------
 
 
 dat.labels <- ddply(dat.merged, .(samp), function(d){
-  sprintf("PeakWidth==%s;FragLength==%s", unique(d$peakwidth), unique(d$fraglength))
+  sprintf("FragLength==%s;PeakWidth==%s", unique(d$peakwidth), unique(d$fraglength))
   # sprintf("FragLength==%s,Peakwidth==%s", unique(d$peakwidth), unique(d$fraglength))
 })
 colnames(dat.labels) <- c("samp", "jlabel")
@@ -97,6 +54,19 @@ ggplot(data = dat.merged,
             parse = TRUE) + 
   scale_x_continuous(limits = c(-200, 200))
 
+
+# Plot best one -----------------------------------------------------------
+
+best <- c("wgEncodeUwDnaseLiverC57bl6MAdult8wksRawDataRep9")
+ggplot(data = subset(dat.merged, 
+                     samp %in% best), 
+       aes(x = distance, y = n.obs, group = strand, colour = strand)) +
+  geom_line() +
+  facet_wrap(~samp) +
+  geom_text(data = subset(dat.labels, samp %in% best), 
+            aes(x = 0, y = 0, label = jlabel), 
+            colour = "black", 
+            parse = TRUE)
 
 # Plot the smallest two ---------------------------------------------------
 
@@ -123,11 +93,31 @@ suspects2 <- c("UwStam_mLiver-DS16853-FC62FJY-3_004",
                "UwStam_mLiver-DS19636-FCD06E9-L006_R1_002")
 
 ggplot(data = subset(dat.merged, 
-                     samp %in% suspects), 
+                     samp %in% suspects2), 
        aes(x = distance, y = n.obs, group = strand, colour = strand)) +
   geom_line() +
   facet_wrap(~samp) +
-  geom_text(data = subset(dat.labels, samp %in% suspects), 
+  geom_text(data = subset(dat.labels, samp %in% suspects2), 
             aes(x = 0, y = 0, label = jlabel), 
             colour = "black", 
             parse = TRUE)
+
+
+# Now do the same for tag distributions -----------------------------------
+
+tcd.cnames <- GetTcdCnames() 
+dat.tcd <- data.frame(matrix(nrow = 0, ncol = length(tcd.cnames)))
+colnames(dat.tcd)
+
+countdist.fname <- "tagCountDistribution.txt"
+for (samp in samp.paths){
+  countdist.base <- basename(samp)
+  countdist.path <- file.path(samp, countdist.fname)
+  dat.temp <- ReadTagCountsDistribution(countdist.path, countdist.base)
+  dat.tcd <- rbind(dat.tcd, dat.temp)
+}
+
+ggplot(data = dat.tcd, aes(x = tags.per.pos, y = frac.pos)) +
+  geom_bar(stat = "identity") +
+  scale_x_continuous(breaks = 1:10, limits = c(0, 10)) +
+  facet_wrap(~samp)
---
+++ scripts/functions/DhsFunctions.R
@@ -0,0 +1,58 @@
+GetPeakwidth <- function(col1){
+  # Given column 1 row 1 of autocorrelation.txt from homer,
+  # get Peakwidth
+  peakwidth <- strsplit(col1, split = '\\.\\.')[[1]][[4]] 
+  peakwidth <- as.numeric(peakwidth)
+  return(peakwidth)
+}
+
+GetFragLength <- function(col1){
+  # given col1 row1 get fraglength from autocorrelation.txt
+  fraglength <- as.numeric(strsplit(col1, split = '\\.\\.')[[1]][[2]])
+  return(fraglength)
+}
+
+Cnames <- function(){
+  # col2 is distance
+  # col2 and col3 just + and - strand
+  return(c("distance", "pos", "neg"))
+}
+
+Cnames.long <- function(){
+  return(c("distance", "n.obs", "strand", "peakwidth", "fraglength", "samp"))
+}
+
+ReadAutocorrelation <- function(f.path, samp.name){
+  # Read Autocorrelation.txt from homer output
+  samp.ac <- read.table(f.path, sep='\t', header=TRUE)
+  peakwidth <- GetPeakwidth(colnames(samp.ac)[1])
+  fraglength <- GetFragLength(colnames(samp.ac)[1])
+  colnames(samp.ac) <- Cnames()  # makes calling vectors easier
+  # column 1 header contains peakwidth info
+  samp.ac.long <- data.frame(distance = rep(samp.ac$distance, 2),
+                             n.obs = c(samp.ac$pos, samp.ac$neg),
+                             strand = c(rep("+", length(samp.ac$pos)), rep("-", length(samp.ac$neg))),
+                             peakwidth = rep(peakwidth, nrow(samp.ac) * 2),
+                             fraglength = rep(fraglength, nrow(samp.ac) * 2),
+                             samp = rep(samp.name, nrow(samp.ac) * 2))
+  cnames <- Cnames.long()
+  colnames(samp.ac.long) <- cnames
+  return(samp.ac.long)
+}
+
+GetTcdCnames <- function(){
+  return(c(GetTcdCnames.partial(), "samp"))
+}
+
+GetTcdCnames.partial <- function(){
+  return(c("tags.per.pos", "frac.pos"))
+}
+
+ReadTagCountsDistribution <- function(f.path, samp.name){
+  samp.tcd <- read.table(f.path, sep='\t', header=TRUE)
+  colnames(samp.tcd) <- GetTcdCnames.partial()
+  samp.tcd.long <- data.frame(tags.per.pos = samp.tcd$tags.per.pos,
+                              frac.pos = samp.tcd$frac.pos,
+                              samp = rep(samp.name, nrow(samp.tcd)))
+  return(samp.tcd.long)
+}
