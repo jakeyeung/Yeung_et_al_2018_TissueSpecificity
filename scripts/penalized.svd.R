4d8a5ed1baaeedd4ae23ff1a226c1f0233969345
Jake Yeung <jake.yeung@epfl.ch>
2015-03-27 20:09:37 +0100

    init

---
+++ scripts/penalized.svd.R
@@ -0,0 +1,14 @@
+# penalized.svd.R
+# Run penalized.svd.R on samples
+
+library(devtools)
+
+dev_mode(on=T)
+
+install_github("jakeyeung/PMA")
+
+# use dev ggplot2 now
+
+# when finished do:
+
+dev_mode(on=F) #and you are back to having stable ggplot2
