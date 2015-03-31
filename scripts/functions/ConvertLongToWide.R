dfd0502e3ec1641bef2db9ab57a01adfc099f205
Jake Yeung <jake.yeung@epfl.ch>
2015-03-31 12:10:14 +0200

    use long.df rather than dat.proj it's a bug.

--- scripts/functions/ConvertLongToWide.R
+++ scripts/functions/ConvertLongToWide.R
@@ -1,5 +1,5 @@
 ConvertLongToWide <- function(long.df, measurement.var = "exprs.transformed"){
-  wide.df <- dcast(dat.proj, gene ~ tissue, value.var = measurement.var)
+  wide.df <- dcast(long.df, gene ~ tissue, value.var = measurement.var)
   # first row is gene name, let's make them rowname and remove first column.
   
   rownames(wide.df) <- wide.df$gene
