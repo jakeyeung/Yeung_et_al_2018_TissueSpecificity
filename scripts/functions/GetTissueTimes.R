162a98e2f7f18c7b2f44df4cc62f07569d72ce4e
Jake Yeung <jake.yeung@epfl.ch>
2015-04-09 13:58:12 +0200

    add functions for the case of "merged", because now my activities matrix has both array and rnaseq together.

--- scripts/functions/GetTissueTimes.R
+++ scripts/functions/GetTissueTimes.R
@@ -20,3 +20,28 @@ GetTimes <- function(samp.names){
   }))
   return(unique(times))
 }
+
+GetTissues.merged <- function(samp.names){
+  # Sampnames of form: Kidney60.array
+  # return NON-unique
+  tissues <- unlist(lapply(samp.names, function(samp.name.full){
+    samp.name <- strsplit(samp.name.full, "[.]")[[1]][[1]]
+    substr(samp.name, 1, nchar(samp.name) - 2)
+  }))
+  return(tissues)
+}
+
+GetTimes.merged <- function(samp.names){
+  # Sampnames of form: Kidney60.array
+  # return NON-unique
+  times <- unlist(lapply(samp.names, function(samp.name.full){
+    samp.name <- strsplit(samp.name.full, "[.]")[[1]][[1]]
+    substr(samp.name, nchar(samp.name) - 1, nchar(samp.name))
+  }))
+  return(as.numeric(times))}
+
+GetExperiments.merged <- function(samp.names){
+  experiments <- unlist(lapply(samp.names, function(samp.name.full){
+    strsplit(samp.name.full, "[.]")[[1]][[2]]
+  }))
+}
