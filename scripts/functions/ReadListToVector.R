e1d911899303f9142e168218e4976dcff8ad2c0f
Jake Yeung <jake.yeung@epfl.ch>
2015-04-28 21:11:21 +0200

    add option to ignore first row

--- scripts/functions/ReadListToVector.R
+++ scripts/functions/ReadListToVector.R
@@ -1,8 +1,12 @@
 # ReadListToVector.R
 
-ReadListToVector <- function(fname){
+ReadListToVector <- function(fname, skip_first_row=FALSE){
   # read file name, one column with no colnames as a vector.
   vec <- read.table(fname)
   vec <- as.character(unlist(vec))
-  return(vec)
+  if (skip_first_row){
+    return(vec[2:length(vec)])
+  } else{
+    return(vec)
+  }
 }
