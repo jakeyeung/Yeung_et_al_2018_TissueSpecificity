1fa952c0198dd2cdbe71dda5d53c1d683d7af1fb
Jake Yeung <jake.yeung@epfl.ch>
2015-04-28 21:07:28 +0200

    add an unregister function

--- scripts/functions/MakeCluster.R
+++ scripts/functions/MakeCluster.R
@@ -4,10 +4,17 @@ MakeCluster <- function(ncores, rscript_args){
   # Ref: http://stackoverflow.com/questions/23540129/what-affects-the-time-to-create-a-cluster-using-the-parallel-package
   if (missing(ncores)){
     ncores <- detectCores()
+    print(paste("Cores:", ncores))
   }
   if (missing(rscript_args)){
     rscript_args = c("--no-init-file", "--no-site-file", "--no-environ")
   }
   cl <- makeCluster(ncores, rscript_args=rscript_args)
   registerDoParallel(cl)
+}
+
+unregister <- function() {
+  # http://stackoverflow.com/questions/25097729/un-register-a-doparallel-cluster
+  env <- foreach:::.foreachGlobals
+  rm(list=ls(name=env), pos=env)
 }
