8a4c4ae1bc592c245e867c20b8580e10c663cba8
Jake Yeung <jake.yeung@epfl.ch>
2015-04-28 21:56:21 +0200

    tools for grepping pesky Rik genes

---
+++ scripts/functions/GrepRikGenes.R
@@ -0,0 +1,22 @@
+# 2014-04-28
+# Sometimes genes like X1700040L02Rik are tricky because they
+# can be X1700040L02Rik or 1700040L02Rik
+# Handle them by grepping them and allow switching between the
+# two forms
+
+GrepRikGenes <- function(gene.list){
+  Xgenes <- gene.list[grep("^X\\w*Rik\\b", gene.list)]
+  return(Xgenes)
+}
+
+RemoveX <- function(Xgenes){
+  genes <- sapply(Xgenes, function(x){
+    return(substr(x, 2, nchar(x)))
+  })
+}
+
+AddX <- function(genes){
+  Xgenes <- sapply(genes, function(string){
+    return(paste0("X", string))
+  })
+}
