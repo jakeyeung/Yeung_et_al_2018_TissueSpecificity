be863627af41d21a3a116bf39cbd4639fe019310
Jake Yeung <jake.yeung@epfl.ch>
2015-04-29 12:01:22 +0200

    First pass at finding AFE. Kind of sloppy.

--- scripts/alternative_first_exons/find_alternative_first_exons.R
+++ scripts/alternative_first_exons/find_alternative_first_exons.R
@@ -14,6 +14,8 @@ source("scripts/functions/LoadAndHandleData.R")
 source("scripts/functions/MixtureModelFunctions.R")
 source("scripts/functions/PlotGeneAcrossTissues.R")
 source("scripts/functions/MakeCluster.R")
+source("scripts/functions/ReadListToVector.R")
+source("scripts/functions/GrepRikGenes.R")
 
 GetGeneNameFromAnnot <- function(annot){
   # from gene_name=RP23-271O17.1;transcript_id=ENSMUST00000193812 retrieve RP23-27017.1
@@ -84,9 +86,13 @@ GetLocationFromAnnotation <- function(bed, gene_name, transcript_id){
   return(paste0(sub$chromosome, ":", sub$start, "-", sub$end))
 }
 
-SubsetBed <- function(bed, gene_name){
+SubsetBed <- function(bed, gene_name, transcript){
   # Subset bed based on grepping annotations from gene name
-  return(bed[grepl(gene_name, bed$annotations), ])
+  if (missing(transcript)){
+    return(bed[grepl(gene_name, bed$annotations), ])  
+  } else if (missing(gene_name)){  
+    return(bed[grepl(transcript, bed$annotations), ])  
+  }
 }
 
 NormalizeBySum <- function(x){
@@ -98,23 +104,29 @@ GetFirst <- function(x){
   return(x[1])
 }
 
-ShannonEntropy <- function(x.vec, scaleit=TRUE, normalizeout=TRUE){
-  if (scaleit){
-    x.vec <- x.vec / sum(x.vec)
-  }
+ShannonEntropy <- function(x.vec, normalizeout=TRUE){
+  # should sum to 1
+  x.vec <- x.vec / sum(x.vec)
   entropy <- 0
   for (x in x.vec){
-    if (x > 0){
-      entropy <- entropy + x * log2(1 / x)
-    }
+    entropy <- entropy + x * log2(1 / x)
   }
-  if (normalizeout){
-    # Divide by max, log2(n)
-    entropy <- entropy / log2(length(x.vec))
-  }
+  entropy <- entropy / log2(length(x.vec))
   return(entropy)
 }
 
+MulitpleStarts <- function(df, min_dist = 500){
+  # Check if df has multiple exons, ddply from bed
+  if (nrow(df) <= 1){
+    return(data.frame(MultiStart = FALSE))
+  }
+  dist <- diff(c(min(df$start), max(df$start)))
+  if (dist >= min_dist){
+    return(data.frame(MultiStart = TRUE))
+  } else{
+    return(data.frame(MultiStart = FALSE))
+  }
+}
 
 # Load RNASeq ---------------------------------------------------
 
@@ -140,6 +152,10 @@ print(paste("Cutoff:", cutoff))
 
 # Main --------------------------------------------------------------------
 
+
+# Load data fix rownames ---------------------------------------------------------------
+
+
 infile <- "data/alternative_exon_usage//Mus_musculus.GRCm38.79.first_exons.coveragebed"
 
 exon.cov <- read.table(infile, header = TRUE, sep = "\t")
@@ -151,11 +167,17 @@ rnames <- make.names(gene.names, unique = TRUE)
 
 rownames(exon.cov) <- rnames
 
+
+# Define annotations and data matrix --------------------------------------
+
+
 # Make a matrix of only coverage values
 bed <- exon.cov[, 1:6]  # we might need it later
 exon.cov <- exon.cov[, 7:ncol(exon.cov)]
 
 plot(density(log2(unlist(exon.cov))))
+
+
 
 
 # Match genes with RNA-Seq ------------------------------------------------
@@ -182,100 +204,105 @@ cov.long <- data.frame(gene = rep(gene.names, ncol(exon.cov)),
 head(cov.long)
 
 
+# Only consider tissue-specific rhythmic genes ----------------------------
+
+# from get_rhythmic_genes_from_list.R
+rhythmic_genes <- ReadListToVector("results/tissue_specific_rhythmic_genes/tissue_specific_rhythmic_genes.genome_wide.cut.txt", 
+                                   HEADER = TRUE)
+# Remove "X" from Rik genes
+Xgenes <- GrepRikGenes(gene.list = rhythmic_genes)
+Xgenes <- RemoveX(Xgenes) 
+
+# Find transcripts whose start sites are significantly far away -----------
+
+# decrease number of genes to consider
+common.genes <- intersect(unique(gene.names), c(rhythmic_genes, Xgenes))  # 1062 genes
+
+bed$gene <- gene.names
+bed.filtered <- ddply(bed, .(gene), MulitpleStarts)
+
+gene.multistarts <- bed.filtered$gene[which(bed.filtered$MultiStart == TRUE)]
+
+# Furhter filter genes
+common.genes <- intersect(common.genes, gene.multistarts)
+
+
+# Filter rhythmic genes with multistarts ----------------------------------
+
+# cov.long.filts <- subset(cov.long, gene %in% common.genes)
 
 # Filter lowly expressed genes --------------------------------------------
 
-cov.long.filt <- subset(cov.long, rnaseq_reads >= 2^cutoff)  # cutoff established in log2 scale
+# cov.long.filt <- subset(cov.long.filts, rnaseq_reads >= 2^cutoff)  # cutoff established in log2 scale
+
+
+# Optionaly do not filter anything ----------------------------------------
+
+cov.long.filt <- cov.long
 
 
 # Normalize exon reads ----------------------------------------------------
 
-cov.long.filt$reads_norm <- cov.long.filt$reads / cov.long.filt$rnaseq_reads
+# cov.long.filt$reads_norm <- cov.long.filt$reads / cov.long.filt$rnaseq_reads
+cov.long.split <- split(cov.long, cov.long$gene)
 
 
 # Average across tissues --------------------------------------------------
-# ** Ddc: clear alternative promoter usage Kidney and Liver rhythmic but not in others.
-# Adam19: example of differential promoter usage (the promoters are close together). Rhythmic in Heart
-# Ahcyl2: a small alternative promoter is used in lung. Not sure if it's definitive. Rhythmic in Lung
-# Chka: no significant alternative promoter. RHythmic in Liver.
-# Slc41a3: alternative promoter used in Adr and Heart. But only heart is rhythmic. Example of both alternative and differential promoter usage?
-# Slc6a6: Rhythmic in WFAT, BFAT, LUNG, Aorta, but no sign of alterntaive promoter usage. Seems it was annotated incorrectly, the second promoter doesn't have any annotated start site.
-# Angptl2: misannotated? Rhythmic in many tissues but no sign of alternative promoter usage.
-# Tshr: no alt promoter usage
-# Pxmp4: no alt promoter usage
-# Pnp: no alt promoter usage
-# Sec14l2: curious Adr triple peak signal, all others are flat. But UCSC doesnt show alt promoter
-# Hdac5: rhythmic in liver only but UCSC doesnt show alt promoter. Unclear.
-# Dtna: "rhyhmic in BFAT, but no sign that this is due to alt promoter. Alt promoter usage noticeable in BS, Cere and Hypo and Lung, however. Differences between heart and BS is obvious.
-# Ppargc1a: small evidence of alt promoter usage. 
-# Ank3: kidney and lung small evidence of alt promoter usage but doesn't match expression profile.
-# ** Sept9: some evidence that BFAT and Liver take the upstream promoter, which are the rhythmic genes.
-# Ncoa7: small evidence of alternative promoter usage linked with rhythmicity. But rhythms are small.
-# Asph: strong evidence of alternative promoter usage (see it in muscle) but BFAT has nothing particularly special.
-# Pnkd: sign that liver may be alternative promoter usage, but it's weak
-# Ngef: sign of alternative promoter usage for kidney and liver, but kidney and liver is weakly rhythmic. Lung is strongly rhythmic.
-# ** Steap3: Liver specific promoter.
-# ** Insig2 clear alt promoter in liver. Strong rhythmics in liver.
-# ** Slc45a3 strong rhythmic in liver and alt promoter in liver.
-# ** Mpzl1: strong rhythmic in liver, small rhythms in other genes. Possible alt promoter? Should check.
 
-# genes.test <- c("Arntl", "Clock", "Insig2", "Dbp", "Ddc", "Sept9", "Steap3", "Slc45a3", "Mpzl1", "Dtna", "Tshr", "Pnp",
-#                 "Hdac5", "Ppargc1a", "Ank3", "Ncoa7", "Hnf4a", "Nr1d1", "Rest")
 # rscript_args <- MakeCluster()
 # ncores <- detectCores()
 # cl <- makeCluster(ncores, rscript_args=rscript_args)
 # # stopCluster(cl)  # if you cannot open connections
 # registerDoParallel(cl)
-doParallel::registerDoParallel(cores = 40)
+doParallel::registerDoParallel(cores = 48)
 start <- Sys.time()
 cov.long.filt.avg <- ddply(cov.long.filt, 
                            .(tissue, transcript),
                            .fun = summarise,
                            reads_norm_avg = mean(reads_norm),
-                           gene = GetFirst(gene),
+                           gene = unique(gene),
                            .parallel = TRUE)
 print(Sys.time() - start)
 
-save(cov.long.filt.avg, file = "data/alternative_exon_usage/cov.long.filt.avg.Robj")
+# save(cov.long.filt.avg, file = "data/alternative_exon_usage/cov.long.filt.avg.Robj")
+# load("data/alternative_exon_usage/cov.long.filt.avg.Robj")
 
-# # Force their sums to 1 ---------------------------------------------------
-# 
-# cov.long.filt.avg$frac_reads_norm <- NormalizeBySum(cov.long.filt.avg$reads_norm_avg)
-# 
-# 
-# # Calculate ShannonEntropy ------------------------------------------------
-# 
-# cov.long.filt.entropy <- ddply(cov.long.filt.avg, .(transcript),
-#                                summarise,
-#                                entropy = ShannonEntropy(frac_reads_norm, normalize = TRUE),
-#                                gene = GetFirst(gene))
-# 
-# 
-# # Sort by entropy ---------------------------------------------------------
-# 
-# print(cov.long.filt.entropy[order(cov.long.filt.entropy$entropy), ])
-# 
-# # Test with Ddc and Insig2 ------------------------------------------------
-# 
-# jgene <- "Dbp"
-# jtrans <- "ENSMUST00000107740"
-# 
-# jgene <- "Insig2"
-# jtrans <- "ENSMUST00000161068"
-# 
-# jgene <- "Ddc"
-# jtrans <- "ENSMUST00000178704"
-# 
-# jgene <- "Hnf4a"
-# jtrans <- "ENSMUST00000143911"
-# 
-# cov.long.sub <- subset(cov.long, gene == jgene)
-# 
-# test <- subset(cov.long.sub, transcript == jtrans)
-# test$normcov <- test$reads / test$rnaseq_reads
-# 
-# ggplot(data = test, aes(x = time, y = reads)) + 
-#   geom_point() + 
-#   geom_line() + 
-#   facet_wrap(~tissue) + 
-#   ggtitle(paste(jgene, jtrans))
+# Calculate ShannonEntropy ------------------------------------------------
+
+cov.long.filt.entropy <- ddply(cov.long.filt.avg, .(transcript),
+                               .fun = summarise,
+                               entropy = ShannonEntropy(reads_norm_avg))
+
+
+# Remove NaNs -------------------------------------------------------------
+
+cov.long.filt.entropy <- cov.long.filt.entropy[which(!is.nan(cov.long.filt.entropy$entropy)), ]
+
+# Sort by entropy ---------------------------------------------------------
+
+print(head(cov.long.filt.entropy[order(cov.long.filt.entropy$entropy), ], n = 50))
+
+# Test with Ddc and Insig2 ------------------------------------------------
+
+jgene <- "Dbp"
+jtrans <- "ENSMUST00000107740"
+
+jgene <- "Insig2"
+jtrans <- "ENSMUST00000161068"
+
+jgene <- "Ddc"
+jtrans <- "ENSMUST00000178704"
+
+jgene <- "Hnf4a"
+jtrans <- "ENSMUST00000143911"
+
+cov.long.sub <- subset(cov.long, gene == jgene)
+
+test <- subset(cov.long.sub, transcript == jtrans)
+test$normcov <- test$reads / test$rnaseq_reads
+
+ggplot(data = test, aes(x = time, y = reads)) + 
+  geom_point() + 
+  geom_line() + 
+  facet_wrap(~tissue) + 
+  ggtitle(paste(jgene, jtrans))
