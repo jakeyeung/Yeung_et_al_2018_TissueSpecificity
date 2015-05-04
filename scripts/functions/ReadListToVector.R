3f674aeba5b36eb60c60d1f85438bb570e7cfa91
Jake Yeung <jake.yeung@epfl.ch>
2015-05-04 08:36:55 +0200

    Read list to vector: allow skipping of header

--- scripts/functions/ReadListToVector.R
+++ scripts/functions/ReadListToVector.R
@@ -1,12 +1,8 @@
 # ReadListToVector.R
 
-ReadListToVector <- function(fname, skip_first_row=FALSE){
+ReadListToVector <- function(fname, HEADER=FALSE){
   # read file name, one column with no colnames as a vector.
-  vec <- read.table(fname)
+  vec <- read.table(fname, header = HEADER)
   vec <- as.character(unlist(vec))
-  if (skip_first_row){
-    return(vec[2:length(vec)])
-  } else{
-    return(vec)
-  }
+  return(vec)
 }
