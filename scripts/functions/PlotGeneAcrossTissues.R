1c7446de551d8dbe0760591e5925c882e4dfc4f7
Jake Yeung <jake.yeung@epfl.ch>
2015-05-18 10:30:15 +0200

    Plot for Tpm across tissues.

--- scripts/functions/PlotGeneAcrossTissues.R
+++ scripts/functions/PlotGeneAcrossTissues.R
@@ -15,3 +15,24 @@ PlotGeneAcrossTissues <- function(dat, jtitle){
     ylab(label = "log2 expression")
   return(m)
 }
+
+PlotTpmAcrossTissues <- function(dat, jtitle, log2.transform=FALSE){
+  library(ggplot2)
+  if (missing(jtitle)){
+    jtitle = unique(dat$gene_name)
+  }
+  if (log2.transform == FALSE){
+    m <- ggplot(dat, aes(x = time, y = tpm)) 
+    jylab <- "TPM expression"
+  } else {
+    m <- ggplot(dat, aes(x = time, y = log2(tpm)))
+    jylab <- "log2 TPM expression"
+  }
+  m <- m + 
+    geom_point() + 
+    geom_line() + 
+    facet_wrap(~tissue) +
+    ggtitle(jtitle) + 
+    ylab(label = jylab)
+  return(m)
+}
