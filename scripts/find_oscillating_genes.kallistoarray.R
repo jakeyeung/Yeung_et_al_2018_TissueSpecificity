b23ea9ba89494cfe1f356ecb5be4ba6e6b868d7c
Jake Yeung <jake.yeung@epfl.ch>
2015-06-19 22:32:43 +0200

    Find rhythmic genes with relative amplitude threshold

--- scripts/find_oscillating_genes.kallistoarray.R
+++ scripts/find_oscillating_genes.kallistoarray.R
@@ -4,17 +4,30 @@
 # to a core clock gene as a cutoff.
 
 library(ggplot2)
-
+library(dplyr)
 # Functions ---------------------------------------------------------------
 
 source("scripts/functions/PlotGeneTpm.R")
 source("scripts/functions/LoadArray.R")
 source("scripts/functions/LoadKallistoGene.R")
 source("scripts/functions/GetTissueTimes.R")
+source("scripts/functions/FitRhythmic.R")
+source("scripts/functions/PlotGeneAcrossTissues.R")
 
-LoadLong <- function(array.path, rna.seq.path, pseudocount = 1e-5){
+FindMostRhythmic <- function(dat, colname="amp", decreasing = TRUE){
+  # return first row after sorting by colname
+  return(dat[order(dat[[colname]], decreasing = decreasing), ][1, ])
+}
+
+LoadLong <- function(array.path, rna.seq.path, scale.factor = 1, pseudocount = 1e-5){
   kallisto.wide <- LoadKallistoGene(rna.seq.path, form = "wide")  # adjusts colnames to match array
   array.wide <- read.table(array.path)
+  # remove rows with negatives (should be 7 of them)
+  problem.genes <- rownames(array.wide[which(apply(array.wide, 1, min) < 0), ])
+  print("Problem genes:")
+  print(problem.genes)
+  good.genes <- setdiff(rownames(array.wide), problem.genes)
+  array.wide <- array.wide[good.genes, ]
   common.genes <- intersect(rownames(kallisto.wide), rownames(array.wide))
   kallisto.sub <- kallisto.wide[common.genes, ]
   array.sub <- array.wide[common.genes, ]
@@ -27,9 +40,9 @@ LoadLong <- function(array.path, rna.seq.path, pseudocount = 1e-5){
   ka.long <- data.frame(gene = c(rep(rownames(kallisto.sub), ncol(kallisto.sub)), rep(rownames(array.sub), ncol(array.sub))),
                         tissue = c(rep(tissues.rnaseq, each = nrow(kallisto.sub)), rep(tissues.array, each = nrow(array.sub))),
                         time = as.numeric(c(rep(times.rnaseq, each = nrow(kallisto.sub)), rep(times.array, each = nrow(array.sub)))),
-                        tpm = c(unlist(kallisto.sub), unlist(array.sub)),
+                        exprs = c(unlist(kallisto.sub), unlist(array.sub)),
                         experiment = c(rep("rnaseq", nrow(kallisto.sub) * ncol(kallisto.sub)), rep("array", nrow(array.sub) * ncol(array.sub))))
-  ka.long$tpm <- log2(ka.long$tpm + pseudocount)
+  ka.long$exprs <- log2(scale.factor * ka.long$exprs + pseudocount)
   return(ka.long)
 }
 
@@ -45,6 +58,54 @@ PlotGeneAcrossTissues2 <- function(dat, jgene){
 
 array.path <- "data/exprs_matrices/array_adj_to_kallisto.slope07.txt"
 rna.seq.path <- "/home/yeung/projects/tissue-specificity/data/kallisto/abundance.genecounts.matrix.txt"
-ka.long <- LoadLong(array.path, rna.seq.path)
+ka.long <- LoadLong(array.path, rna.seq.path, scale.factor = 100, pseudocount = 1)
 
-PlotGeneAcrossTissues2(ka.long, "Arntl")
+PlotGeneAcrossTissues(subset(ka.long, gene == "Arntl"))
+
+
+# Fit rhythmic ------------------------------------------------------------
+
+fits <- FitRhythmicDatLong(ka.long)
+
+
+# Find most rhythmic gene: use this as relative amplitude for othe --------
+
+max.pval <- 1e-5
+fits.mostrhythmic <- fits %>%
+  group_by(tissue) %>%
+  filter(pval <= max.pval) %>%
+  do(FindMostRhythmic(.)) %>%
+  data.frame(.)
+
+rownames(fits.mostrhythmic) <- fits.sort$tissue  # indexing
+
+
+# Get amplitude relative to max amp ---------------------------------------
+
+GetAmpRelToMax <- function(dat, fits.mostrhythmic){
+  tissue <- as.character(dat$tissue[1])
+  amp.max <- fits.mostrhythmic[tissue, ]$amp 
+  dat$relamp <- dat$amp / amp.max
+  return(dat)
+}
+
+fits.relamp <- fits %>%
+  group_by(tissue) %>%
+  do(GetAmpRelToMax(., fits.mostrhythmic))
+
+# List rhythmic genes -----------------------------------------------------
+
+min.relamp <- 0.3  # fraction of largest amplitude of most rhythmic gene
+
+fits.rhyth <- subset(fits.relamp, pval <= max.pval & relamp >= min.relamp)
+
+rhythmic.genes <- unique(fits.rhyth$gene)
+
+print(length(rhythmic.genes))
+
+# number of rhythmic genes in each tissue
+rhyth.by.tiss <- fits.rhyth %>%
+  group_by(tissue) %>%
+  summarise(length(gene))
+
+print(rhyth.by.tiss)
--- scripts/functions/FitRhythmic.R
+++ scripts/functions/FitRhythmic.R
@@ -1,7 +1,7 @@
-FitRhythmic <- function(df, T = 24){
-  # Fit rhythmic model to long df. Expect df to be per tissue per gene.
+FitRhythmic <- function(dat, T = 24){
+  # Fit rhythmic model to long dat. Expect dat to be per tissue per gene.
   # use lapply and ddply to vectorize this code.
-  # df needs columns: tissue time experiment exprs
+  # dat needs columns: tissue time experiment exprs
   
   
   # Get parameters from complex model: used later
@@ -13,22 +13,22 @@ FitRhythmic <- function(df, T = 24){
                 b = model.params[4]))
   }
   
-  tissue <- unique(df$tissue)
+  tissue <- unique(dat$tissue)
   w = 2 * pi / T
   # Expect columns exprs and time
-  rhyth.fit <- lm(exprs ~ 0 + experiment + cos(w * time) + sin(w * time), data = df)
-  flat.fit <- lm(exprs ~ 0 + experiment, data = df)
+  rhyth.fit <- lm(exprs ~ 0 + experiment + cos(w * time) + sin(w * time), data = dat)
+  flat.fit <- lm(exprs ~ 0 + experiment, data = dat)
   compare.fit <- anova(flat.fit, rhyth.fit)
   pval <- compare.fit["Pr(>F)"][[1]][2]
   model.params <- GetParamsRhythModel(rhyth.fit)  # y = experimentarray + experimentrnaseq + a cos(wt) + b sin(wt)
   amp <- sqrt(model.params$a ^ 2 + model.params$b ^ 2)
   phase.rad <- atan2(model.params$b, model.params$a)
   phase.time <- (phase.rad / w) %% T
-  df.out <- data.frame(tissue = tissue, 
+  dat.out <- data.frame(tissue = tissue, 
                        cos.part = model.params$a, sin.part = model.params$b, 
                        amp = amp, phase = phase.time, pval = pval,
                        int.array = model.params$intercept.array, int.rnaseq = model.params$intercept.rnaseq)
-  return(df.out)
+  return(dat.out)
 }
 
 FitRhythmicDatLong <- function(dat.long){
@@ -44,3 +44,4 @@ FitRhythmicDatLong <- function(dat.long){
   print(Sys.time() - start)
   return(dat.fitrhyth)
 }
+
