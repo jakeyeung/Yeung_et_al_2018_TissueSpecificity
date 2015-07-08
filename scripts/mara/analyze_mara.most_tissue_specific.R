c5dbe1d2700d3a6bad04c9a982af656353736240
Jake Yeung <jake.yeung@epfl.ch>
2015-07-08 18:13:58 +0200

    print activities with tissue-specific activities

---
+++ scripts/mara/analyze_mara.most_tissue_specific.R
@@ -0,0 +1,54 @@
+# Jake Yeung
+# analyze_mara.R
+# 2015-06-23
+setwd("/home/yeung/projects/tissue-specificity")
+
+# Functions ---------------------------------------------------------------
+
+source("scripts/functions/GetTissueTimes.R")
+source("scripts/functions/ActivitiesMergedFunctions.R")
+source("scripts/functions/PlotFunctions.R")
+source("scripts/functions/SvdFunctions.R")
+source("scripts/functions/PlotActivitiesFunctions.R")
+source("scripts/functions/LoadActivitiesLong.R")
+
+library(dplyr)  # problems with plyr old package
+library(reshape2)
+
+# Load --------------------------------------------------------------------
+
+indir <- "/home/yeung/projects/tissue-specificity/results/MARA/MARA_motevo_with_se.redo/activities"
+act.long <- LoadActivitiesLong(indir)
+
+
+# Which ones are most rhythmic? -------------------------------------------
+act.fit <- act.long %>%
+  group_by(tissue, gene) %>%
+  do(.data = ., FitRhythmicWeighted(dat = ., T = 24, intercepts = TRUE))
+
+
+# Which are most tissue-specific? -----------------------------------------
+
+act.flat <- act.fit %>%
+  group_by(gene) %>%
+  summarise(Range = diff(range(rnaseq.int))) %>%
+  arrange(desc(Range))
+
+
+# pdf("plots/activities/tissue_specific_activities.pdf")
+# for(m in act.flat$gene){
+#   print(PlotActivitiesWithSE(subset(act.long, gene == m)))
+# }
+# dev.off()
+
+
+# And by rhythms? ---------------------------------------------------------
+
+act.rhyth <- act.fit %>%
+  group_by(gene) %>%
+  summarise(min.pval = min(pval)) %>%
+  arrange(min.pval)
+
+pdf("plots/activities/rhythmic_activities.pdf")
+mapply(function(jgene, jtitle) print(PlotActivitiesWithSE(subset(act.long, gene == jgene), jtitle = jtitle)), act.rhyth$gene, paste(act.rhyth$gene, signif(act.rhyth$min.pval, digits=3)))
+dev.off()
