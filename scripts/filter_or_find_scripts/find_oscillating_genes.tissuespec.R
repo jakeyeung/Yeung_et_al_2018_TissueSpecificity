7d3401df9494a3c682a7a42bce6a260a799d281a
Jake Yeung <jake.yeung@epfl.ch>
2015-07-16 08:23:10 +0200

    plot kidney versus liver-specific genes

--- scripts/filter_or_find_scripts/find_oscillating_genes.tissuespec.R
+++ scripts/filter_or_find_scripts/find_oscillating_genes.tissuespec.R
@@ -81,94 +81,106 @@ dat.filt <- subset(ka.long, gene %in% tissue.spec)
 tpm.genes <- unique(tpm.filt$gene)
 
 outpath <- "plots/tissue_specific_rhythmic_genes/liver_v_kidney.pdf"
-print(paste("Plotting liver vs kidney to", outpath))
-start.time <- Sys.time()
-pdf(outpath)
-for (jgene in tissue.spec.amp){
-  if (!jgene %in% tpm.genes) next
-  print(PlotGeneAcrossTissues(subset(dat.filt, gene == jgene)))
-  print(PlotTpmAcrossTissues(subset(tpm.filt, gene_name == jgene), log2.transform = FALSE))
+if (!file.exists(outpath)){
+  print(paste("Plotting liver vs kidney to", outpath))
+  start.time <- Sys.time()
+  pdf(outpath)
+  for (jgene in tissue.spec.amp){
+    if (!jgene %in% tpm.genes) next
+    print(PlotGeneAcrossTissues(subset(dat.filt, gene == jgene)))
+    print(PlotTpmAcrossTissues(subset(tpm.filt, gene_name == jgene), log2.transform = FALSE))
+  }
+  dev.off()
+  print(Sys.time() - start.time) 
 }
-dev.off()
-print(Sys.time() - start.time)
 
+
+# Plot gene names ---------------------------------------------------------
+
+outpath.txt <- "plots/tissue_specific_rhythmic_genes/liver_v_kidney.txt"
+sink(file = outpath.txt)
+for (jgene in tissue.spec.amp){
+  cat(jgene)
+  cat("\n")
+}
+sink()
 
 # Plot examples -----------------------------------------------------------
 
-# by amp
-jgene <- "Dnmt3b"
-jgene <- "BC029214"
-jgene <- "Lrfn3"
-jgene <- "Atf5"
-jgene <- "Gfod2"
-jgene <- "Mafb"
-jgene <- "Ddit4"
-jgene <- "Gys2"  # good dist
-jgene <- "Eps8l2"  # hnf regulated? Adr spikey. Almost identical DHS sites btw kid and liv
-jgene <- "Slc25a47"
-jgene <- "Fads3"
-jgene <- "Mapk15"
-jgene <- "Pla2g12a"
-jgene <- "Tfec"
-jgene <- "Pcp4l1"  # OK distance
-jgene <- "Ggt6"
-jgene <- "Maff"
-jgene <- "Homer2"  # distance good but lowly expressed
-jgene <- "Crip2"
-jgene <- "Zc3h12d"  # possible candidate
-jgene <- "Flcn"
-jgene <- "Rbm12b1"
-jgene <- "Pxmp4"
-jgene <- "Uck2"
-jgene <- "Rcan1"  # good candidate for alternative promoter
-jgene <- "Rnd1"
-jgene <- "Gpr97"
-jgene <- "Slc38a2"  # good distance with eRNA evidence
-jgene <- "Neu3"  # good distance with ERNA evidence
-jgene <- "Spcs2"  # neu3's neighbour
-jgene <- "Rad9a"  
-jgene <- "Ppp1ca"  # Rad9a's neighbhour
-jgene <- "Pik3ap1"  # good candidate
-jgene <- "Hp"
-jgene <- "Steap3"
-jgene <- "Cep44"
-
-# by pval
-jgene <- "Slc44a1"  # good distance + eRNA evidence
-jgene <- "Hsd3b7"
-jgene <- "Rbm12b1"
-jgene <- "Sco2"
-jgene <- "Pdgfc"  # good distance, no eRNA evidence
-jgene <- "Tjp3"
-jgene <- "Repin1"
-jgene <- "Fbxo6"
-jgene <- "Slc4a4"  # good distance, alt promoter, has eRNA
-jgene <- "Lgals9"
-jgene <- "Nhlrc2"  # the double promoter
-jgene <- "Lrp5"  # Adr + Liver rhythmic, maybe a candidate but it's muddy
-
-
-PlotGeneAcrossTissues(subset(ka.long, gene == jgene))
-PlotTpmAcrossTissues(subset(tpm.filt, gene_name == jgene), log2.transform = TRUE)
+# # by amp
+# jgene <- "Dnmt3b"
+# jgene <- "BC029214"
+# jgene <- "Lrfn3"
+# jgene <- "Atf5"
+# jgene <- "Gfod2"
+# jgene <- "Mafb"
+# jgene <- "Ddit4"
+# jgene <- "Gys2"  # good dist
+# jgene <- "Eps8l2"  # hnf regulated? Adr spikey. Almost identical DHS sites btw kid and liv
+# jgene <- "Slc25a47"
+# jgene <- "Fads3"
+# jgene <- "Mapk15"
+# jgene <- "Pla2g12a"
+# jgene <- "Tfec"
+# jgene <- "Pcp4l1"  # OK distance
+# jgene <- "Ggt6"
+# jgene <- "Maff"
+# jgene <- "Homer2"  # distance good but lowly expressed
+# jgene <- "Crip2"
+# jgene <- "Zc3h12d"  # possible candidate
+# jgene <- "Flcn"
+# jgene <- "Rbm12b1"
+# jgene <- "Pxmp4"
+# jgene <- "Uck2"
+# jgene <- "Rcan1"  # good candidate for alternative promoter
+# jgene <- "Rnd1"
+# jgene <- "Gpr97"
+# jgene <- "Slc38a2"  # good distance with eRNA evidence
+# jgene <- "Neu3"  # good distance with ERNA evidence
+# jgene <- "Spcs2"  # neu3's neighbour
+# jgene <- "Rad9a"  
+# jgene <- "Ppp1ca"  # Rad9a's neighbhour
+# jgene <- "Pik3ap1"  # good candidate
+# jgene <- "Hp"
+# jgene <- "Steap3"
+# jgene <- "Cep44"
+# 
+# # by pval
+# jgene <- "Slc44a1"  # good distance + eRNA evidence
+# jgene <- "Hsd3b7"
+# jgene <- "Rbm12b1"
+# jgene <- "Sco2"
+# jgene <- "Pdgfc"  # good distance, no eRNA evidence
+# jgene <- "Tjp3"
+# jgene <- "Repin1"
+# jgene <- "Fbxo6"
+# jgene <- "Slc4a4"  # good distance, alt promoter, has eRNA
+# jgene <- "Lgals9"
+# jgene <- "Nhlrc2"  # the double promoter
+# jgene <- "Lrp5"  # Adr + Liver rhythmic, maybe a candidate but it's muddy
+# 
+# 
+# PlotGeneAcrossTissues(subset(ka.long, gene == jgene))
+# PlotTpmAcrossTissues(subset(tpm.filt, gene_name == jgene), log2.transform = TRUE)
 
 
 # Plot candidates ---------------------------------------------------------
 
-candidates <- 
-  c("Slc44a1",
-"Pik3ap1",
-"Neu3",
-"Slc38a2",
-"Rcan1",
-"Slc4a4",
-"Gys2",
-"Homer2",
-"Zc3h12d",
-"Pdgfc")
-
-pdf("plots/tissue_specific_rhythmic_genes/candidates_4c_for_jerome.pdf")
-for (g in candidates){
-  print(PlotGeneAcrossTissues(subset(ka.long, gene == g)))
-  print(PlotTpmAcrossTissues(subset(tpm.filt, gene_name == g & tissue %in% c("Liver", "Kidney")), log2.transform = FALSE))
-}
-dev.off()
+# candidates <- 
+#   c("Slc44a1",
+# "Pik3ap1",
+# "Neu3",
+# "Slc38a2",
+# "Rcan1",
+# "Slc4a4",
+# "Gys2",
+# "Homer2",
+# "Zc3h12d",
+# "Pdgfc")
+# 
+# pdf("plots/tissue_specific_rhythmic_genes/candidates_4c_for_jerome.pdf")
+# for (g in candidates){
+#   print(PlotGeneAcrossTissues(subset(ka.long, gene == g)))
+#   print(PlotTpmAcrossTissues(subset(tpm.filt, gene_name == g & tissue %in% c("Liver", "Kidney")), log2.transform = FALSE))
+# }
+# dev.off()
