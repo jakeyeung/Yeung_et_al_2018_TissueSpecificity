4512fb827e53623f7f4917c351d817719ccd02b8
Jake Yeung <jake.yeung@epfl.ch>
2015-08-11 10:47:24 +0200

    Load output from MARA on DHS signal

--- scripts/functions/LoadActivitiesLong.R
+++ scripts/functions/LoadActivitiesLong.R
@@ -23,3 +23,20 @@ LoadActivitiesLong <- function(indir, act.file="activities.all", se.file="standa
                          experiment = rep(experiments, each = nrow(merged.act)))
   return(act.long)
 }
+
+LoadActivitiesLongDhs <- function(indir, act.file, se.file){
+  # expect columns to be just tissues (no time).
+  source("~/projects/tissue-specificity/scripts/functions/ActivitiesMergedFunctions.R")
+  source("~/projects/tissue-specificity/scripts/functions/GetTissueTimes.R")
+  merged.act <- read.table(file.path(indir, act.file))
+  merged.se <- read.table(file.path(indir, se.file))
+  
+  # Create long -------------------------------------------------------------
+  
+  tissues <- colnames(merged.act)
+  act.long <- data.frame(gene = rep(rownames(merged.act), ncol(merged.act)),
+                         tissue = rep(tissues, each = nrow(merged.act)),
+                         exprs = as.numeric(unlist(merged.act)),
+                         se = as.numeric(unlist(merged.se)))
+  return(act.long)
+}
