40373b6422a6ff2f697f5d0a1f758634277d6db2
Jake Yeung <jake.yeung@epfl.ch>
2015-09-06 21:50:50 +0200

    Filter by mixture of gamma functions. Lots of tweaking for initial values but I think now it's ok

--- scripts/dhs/dhs_merged_find_peaks.R
+++ scripts/dhs/dhs_merged_find_peaks.R
@@ -9,9 +9,16 @@ fname <- file.path(args[1])
 # fname <- "data/beds/merge/ucsc_names/Liver.dhs.merged.bed"
 outdir <- file.path(args[2])  # no extension, add it in later
 # outdir <- "data/beds/merge/Liver.cutoff"
-outbed <- file.path(args[3])
-init_mu1 <- as.numeric(args[4])
-init_mu2 <- as.numeric(args[5])
+outbed <- file.path(args[3])  # full path yo
+filter_counts <- as.numeric(args[4])
+init_mu1 <- as.numeric(args[5])
+init_mu2 <- as.numeric(args[6])
+
+fname.split <- strsplit(basename(fname), "\\.")[[1]]
+fname.base <- paste(fname.split[1:(length(fname.split) - 1)], collapse = ".")
+
+print(fname.base)
+print(paste0(outdir, '/', fname.base, ".pdf"))
 
 if (is.na(outbed)){
   outbed <- file.path(outdir, "filtered.bed")
@@ -52,18 +59,31 @@ dat <- read.table(fname, header = FALSE)
 
 # Find peaks from merged bam files ----------------------------------------
 
-counts <- dat$V4[which(dat$V4 > 0)]
+counts <- dat$V4[which(dat$V4 > 1)]
 counts <- sample(counts, 0.01 * length(counts))  # make curve a little smoother
 
-plot(density(log2(counts)))
 
 # outdir <- "data/beds/merge/Liver.cutoff.pdf"
 print("Begin: fit mixture model")
 start <- Sys.time()
 cutoff.log2 <- FindCutoff(x = log2(counts), lambdas = c(0.7, 0.3), mus = c(init_mu1, init_mu2), outdir=outdir)
+# fit gammae
+gscale <- 50
+gmeans <- c(3, 12)
+glambdas <- c(0.8, 0.2)
+mixmdl <- gammamixEM(log2(counts), lambda = glambdas, alpha = c(gscale, gscale), beta = c(gmeans[1] / gscale, gmeans[2] / gscale))
+save(mixmdl, file = file.path(outdir, "gammamdl.Robj"))
+# find cutoff for gamma
+cutoff.gamma.log2 <- PlotGammaMixmdl(mixmdl, log2(counts), savedir = outdir)
+cutoff.gamma <- 2 ^ cutoff.gamma.log2
 print("End: fit mixture model")
 print(Sys.time() - start)
 
+pdf(paste0(outdir, "/", fname.base, ".pdf"))
+plot(density(log2(counts)))
+abline(v = cutoff.log2$maximum)
+plot(mixmdl, which = 2)
+dev.off()
 
 cutoff <- 2^cutoff.log2$maximum
 print(paste("Cutoff:", cutoff, "log2 Cutoff:", cutoff.log2$maximum))
@@ -74,3 +94,7 @@ print(paste("Cutoff:", cutoff, "log2 Cutoff:", cutoff.log2$maximum))
 dat.filt <- dat[which(dat$V4 >= cutoff), ]
 write.table(dat.filt, file = outbed, quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
 
+# Cutoff by making all data that do not pass cutoffs to zero. This makes merging easier.
+dat.filt <- dat
+dat.filt$V4[which(dat.filt$V4 < cutoff.gamma)] <- 0
+write.table(dat.filt, file = file.path(outdir, paste0(fname.base, ".gamma_filtered.bed")), quote = FALSE, sep = "\t", row.names = FALSE, col.names = FALSE)
--- scripts/functions/MixtureModelFunctions.R
+++ scripts/functions/MixtureModelFunctions.R
@@ -27,16 +27,35 @@ FindCutoff <- function(x, lambdas, mus, k = 2, outdir = FALSE){
     save(mixmdl, file = file.path(outdir, "mixmdl.Robj"))
     sink(file.path(outdir, "cutoff.txt"))
     cat(2^cutoff$maximum)
+    cat("\n")
+    cat(cutoff$maximum)
     sink()
   }
   return(cutoff)
 }
 
+GammaMixmdl <- function(x, mixmdl, i = 1){
+  return(dgamma(x, shape = mixmdl$gamma.pars[1, i], scale = mixmdl$gamma.pars[2, i]))
+}
+
+FindCutoff2 <- function(xspace, mixmdl){
+  # Find cutoff from mixmdl: use this to get Gamma functions
+  # BEGIN: get intervals
+  max1 <- optimize(f = GammaMixmdl, interval = range(xspace), mixmdl = mixmdl, i = 1, maximum = TRUE)$maximum
+  max2 <- optimize(f = GammaMixmdl, interval = range(xspace), mixmdl = mixmdl, i = 2, maximum = TRUE)$maximum
+  cutoff <- optimize(f = ShannonEntropyMixMdl, interval = range(max1, max2), mixmdl = mixmdl, tol = 0.0001, maximum = TRUE)
+  return(cutoff$maximum)
+}
+
 ShannonEntropyMixMdl <- function(x, mixmdl){
-  if (length(mixmdl$lambda) == 2){
-    p1 <- PredictFromMM(mixmdl, x)
-  } else if (length(mixmdl$lambda) == 3){
-    p1 <- PredictFromMM3(mixmdl, x)
+  if (mixmdl$ft == "normalmixEM"){
+    if (length(mixmdl$lambda) == 2){
+      p1 <- PredictFromMM(mixmdl, x)
+    } else if (length(mixmdl$lambda) == 3){
+      p1 <- PredictFromMM3(mixmdl, x)
+    }
+  } else if (mixmdl$ft == "gammamixEM"){
+    p1 <- PredictFromMMGamma(mixmdl, x)
   }
   p2 <- 1 - p1
   entropy <- 0
@@ -46,16 +65,22 @@ ShannonEntropyMixMdl <- function(x, mixmdl){
   return(entropy)
 }
 
-GetDensity <- function(mixmdl, x, i){
+GetDensity <- function(mixmdl, x, i, method = "norm"){
   # Get density from a single distribution, given by i
-  density.i <- mixmdl$lambda[i] * dnorm(x, mean = mixmdl$mu[i], sd = mixmdl$sigma[i])
+  if (method == "norm"){
+    density.i <- mixmdl$lambda[i] * dnorm(x, mean = mixmdl$mu[i], sd = mixmdl$sigma[i])
+  } else if (method == "gamma"){
+    density.i <- mixmdl$lambda[i] * dgamma(x, shape = mixmdl$gamma.pars[1, i], scale = mixmdl$gamma.pars[2, i])
+  } else {
+    warning("Method != norm or gamma")
+  }
 }
 
-MixtureDensity <- function(mixmdl, x){
+MixtureDensity <- function(mixmdl, x, method = "norm"){
   # http://www.r-bloggers.com/fitting-mixture-distributions-with-the-r-package-mixtools/
   # estimates density kernel from mixture model as a function of x
   # p(x) = lambda[1] n(x; mu[1], sigma[1]) + lambda[2] n(x; mu[2], sigma[2])
-  p.of.x <- GetDensity(mixmdl, x, 1) + GetDensity(mixmdl, x, 2)
+  p.of.x <- GetDensity(mixmdl, x, 1, method) + GetDensity(mixmdl, x, 2, method)
 }
 
 PredictFromMM <- function(mixmdl, x){
@@ -66,6 +91,13 @@ PredictFromMM <- function(mixmdl, x){
   # Assumes mixture model of two gaussians
   density.1 <- GetDensity(mixmdl, x, 1)
   p.of.x <- MixtureDensity(mixmdl, x)
+  p1.given.x <- density.1 / p.of.x
+  return(p1.given.x)
+}
+
+PredictFromMMGamma <- function(mixmdl, x){
+  density.1 <- GetDensity(mixmdl, x, 1, method = "gamma")
+  p.of.x <- MixtureDensity(mixmdl, x, method = "gamma")
   p1.given.x <- density.1 / p.of.x
   return(p1.given.x)
 }
@@ -87,3 +119,56 @@ PredictFromMM3 <- function(mixmdl, x){
   return(p1.given.x)
 }
 
+PlotGammaMixmdl <- function(mixmdl, counts, savedir = FALSE){
+  # mixmdl from mixtools Gamma functions
+  # counts is in log2
+  shape1 = mixmdl$gamma.pars[1, 1]
+  scale1 = mixmdl$gamma.pars[2, 1]
+  shape2 = mixmdl$gamma.pars[1, 2]
+  scale2 = mixmdl$gamma.pars[2, 2]
+  xmin = min(counts)
+  xmax = max(counts)
+  xspace = seq(from = xmin, to = xmax, length.out = length(counts))
+  # print(paste0("Shape: ", shape1, "Scale: ", scale1))
+	# plot(xspace, density(counts))
+  # print('hi')
+  # plot(xspace, density(counts))
+  g1 <- mixmdl$lambda[1] * dgamma(xspace, shape = shape1, scale = scale1)
+  g2 <- mixmdl$lambda[2] * dgamma(xspace, shape = shape2, scale = scale2)
+  g1g2 <- g1 + g2
+  cutoff <- FindCutoff2(xspace, mixmdl)
+  if (savedir != FALSE){
+    pdf(file.path(savedir, "gamma_plot.pdf"))
+    plot(density(counts))
+  	lines(xspace, g1, type = 'l', col = 'red')
+  	lines(xspace, g2, type = 'l', col = 'red')
+    lines(xspace, g1g2, pch = "-", col = 'blue')
+    abline(v = cutoff)
+    plot(mixmdl)
+    dev.off()
+  } else {
+    plot(density(counts))
+    lines(xspace, g1, type = 'l', col = 'red')
+    lines(xspace, g2, type = 'l', col = 'red')
+    lines(xspace, g1g2, pch = "-", col = 'blue')
+    abline(v = cutoff)
+  }
+  if (savedir != FALSE){
+    sink(file.path(savedir, "cutoff_gamma.txt"))
+    cat(2^cutoff)
+    cat("\n")
+    cat(cutoff)
+    sink()
+  }
+  return(cutoff)
+}
+
+PlotGammaDist <- function(mixmdl, counts){
+  # from http://stackoverflow.com/questions/17450591/fit-gamma-mixture-to-fertility-schedule-in-r
+  print(paste0("Shape: ", mixmdl$gamma.pars[1], "Scale: ", mixmdl$gamma.pars[2]))
+  d3 <- function(x) mixmdl$lambda[1]*dgamma(x, mixmdl$gamma.pars[1], 1/mixmdl$gamma.pars[2]) + mixmdl$lambda[2]*dgamma(x, mixmdl$gamma.pars[3], 1/mixmdl$gamma.pars[4])
+  x <- seq(min(counts), max(counts), 0.001)
+  plot(x, d3(x), col = 'red', pch = ".")
+  # hist(counts, col="pink", add=T, freq=F, breaks=10000)
+  lines(density(counts), col = 'blue')
+}
