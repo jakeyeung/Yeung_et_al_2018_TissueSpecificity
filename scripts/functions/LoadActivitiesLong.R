6bd487396b988965c66b030c69ce22d3b53d85b0
Jake Yeung <jake.yeung@epfl.ch>
2015-09-14 11:10:42 +0200

    add shorten motif names

--- scripts/functions/LoadActivitiesLong.R
+++ scripts/functions/LoadActivitiesLong.R
@@ -1,6 +1,7 @@
-LoadActivitiesLong <- function(indir, act.file="activities.all", se.file="standarderrors.all"){
+LoadActivitiesLong <- function(indir, act.file="activities.all", se.file="standarderrors.all", shorten.motif.name=FALSE){
   source("~/projects/tissue-specificity/scripts/functions/ActivitiesMergedFunctions.R")
   source("~/projects/tissue-specificity/scripts/functions/GetTissueTimes.R")
+  source("~/projects/tissue-specificity/scripts/functions/RemoveP2Name.R")
   merged.act <- read.table(file.path(indir, act.file))
   merged.se <- read.table(file.path(indir, se.file))
   
@@ -14,6 +15,10 @@ LoadActivitiesLong <- function(indir, act.file="activities.all", se.file="standa
   tissues <- GetTissues.merged(colnames(merged.act))
   times <- GetTimes.merged(colnames(merged.act))
   experiments <- GetExperiments.merged(colnames(merged.act))
+  
+  if (shorten.motif.name){
+    rownames(merged.act) <- sapply(rownames(merged.act), RemoveP2Name)
+  }
   
   act.long <- data.frame(gene = rep(rownames(merged.act), ncol(merged.act)),
                          tissue = rep(tissues, each = nrow(merged.act)),
