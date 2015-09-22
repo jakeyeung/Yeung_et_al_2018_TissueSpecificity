aee3f700137b7768895d5be255bb01b683d09eea
Jake Yeung <jake.yeung@epfl.ch>
2015-09-22 08:55:40 +0200

    Move functions to separate file

--- scripts/fourier/total_variance.R
+++ scripts/fourier/total_variance.R
@@ -14,25 +14,7 @@ source("scripts/functions/FitRhythmic.R")
 source("scripts/functions/PlotFunctions.R")
 source("scripts/functions/PlotGeneAcrossTissues.R")
 source("scripts/functions/LoadArray.R")
-
-GetTopGenesByPeriod <- function(dat, jperiod){
-  # dat from dat.complex.all_T
-  dat.sub <- subset(dat, period == jperiod)
-  return(dat.sub[order(Mod(dat.sub$exprs.transformed), decreasing = TRUE), ])
-}
-
-OrderDecreasing <- function(dat, jfactor, jval){
-  # Reorder factors by decreasing value of jval
-  dat[[jfactor]] <- factor(dat[[jfactor]], levels = dat[[jfactor]][order(dat[[jval]], decreasing = TRUE)])
-  return(dat)
-}
-
-GetIndex <- function(x){
-  # Get ith estimate given vector
-  # make a hash table
-  x.o <- order(x, decreasing = TRUE)
-  sapply(x, function(y) which(x.o == which(x == y)))
-}
+source("scripts/functions/VarianceFunctions.R")
 
 # Load data ---------------------------------------------------------------
 
