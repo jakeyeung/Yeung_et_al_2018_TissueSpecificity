df8a0bacd48e433f3fae33633c8bd685df3f8ee5
Jake Yeung <jake.yeung@epfl.ch>
2015-09-23 13:46:36 +0200

    plot replicates over time

--- scripts/filter_or_find_scripts/find_problematic_genes.R
+++ scripts/filter_or_find_scripts/find_problematic_genes.R
@@ -50,7 +50,7 @@ ggplot(subset(dat.24var, gene == jgene), aes(x = time.m, y = Var)) + geom_bar(st
 
 dat.maxvar <- dat.24var %>%
   group_by(gene, tissue, experiment) %>%
-  summarise(max.Var = max(Var))
+  summarise(max.Var = median(Var))
 
 AssignIndex <- function(x){
   return(seq(length(x)))
@@ -63,8 +63,9 @@ dat.maxvar <- dat.maxvar %>%
 
 # Plot distribution in histograms -----------------------------------------
 
-ggplot(dat.maxvar, aes(x = max.Var)) + geom_histogram(binwidth = 0.1) + facet_wrap(~tissue) + scale_x_log10()
-ggplot(dat.maxvar, aes(x = max.Var)) + stat_ecdf() + facet_wrap(~tissue)
+# ggplot(dat.maxvar, aes(x = max.Var)) + geom_histogram(binwidth = 0.1) + facet_wrap(~tissue) + scale_x_log10()
+# ggplot(dat.maxvar, aes(x = max.Var)) + stat_ecdf() + facet_wrap(~tissue)
+ggplot(dat.maxvar, aes(x = max.Var, colour = tissue, fill = tissue)) + stat_ecdf()
 
 
 # By entropy --------------------------------------------------------------
@@ -75,6 +76,28 @@ dat.entropy <- dat.24var %>%
 
 ggplot(dat.entropy, aes(x = H)) + geom_histogram(binwidth = 0.1) + facet_wrap(~tissue)
 ggplot(dat.entropy, aes(x = H)) + stat_ecdf() + facet_wrap(~tissue)
+
+
+# Plot biological replicates ----------------------------------------------
+
+MergeRows <- function(dat){
+  # merge two rows into one to plot biological replicates
+  dat.out <- data.frame(exprs1 = dat$exprs[1], exprs2 = dat$exprs[2])
+  return(dat.out)
+}
+
+# 8 minutes
+dat.reps <- subset(dat.long, experiment == "rnaseq") %>%
+  group_by(gene, time.m, tissue, experiment) %>%
+  do(MergeRows(.))
+head(dat.reps)
+
+
+# Plot replicates across tissues ------------------------------------------
+
+dat.reps$time.m <- factor(dat.reps$time.m)
+cbPalette <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+ggplot(dat.reps, aes(x = exprs1, y = exprs2, colour = time.m)) + geom_point(alpha = 0.5) + facet_wrap(~tissue) + scale_colour_manual(values=cbPalette)
 
 
 # Plot top 5 for each tissue ----------------------------------------------
