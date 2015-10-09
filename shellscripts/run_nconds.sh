b5f0332adf0a35553e21535cc6bfc4fbc1f374b3
Jake Yeung <jake.yeung@epfl.ch>
2015-10-09 21:14:15 +0200

    Rrename output

--- shellscripts/run_nconds.sh
+++ shellscripts/run_nconds.sh
@@ -6,12 +6,12 @@
 
 datlongdir="/home/yeung/projects/tissue-specificity/data/nconds2/datlong_11_tiss_by_gene"
 chunkdir="/home/yeung/projects/tissue-specificity/data/nconds2/mats_11_tiss_max_3_chunks_20000"
-outmain="/home/yeung/projects/tissue-specificity/data/nconds2/fits_11_tiss_chunks.max_3_test.20000"
+outmain="/home/yeung/projects/tissue-specificity/data/nconds2/fits_11_tiss_chunks.max_3_test.20000.redo"
 runscript="/home/yeung/projects/tissue-specificity/scripts/nconds/load_chunk_datgene_run_nconds.R"
 
 [[ ! -e $runscript ]] && echo "$runscript not found, exiting" && exit 1
 [[ ! -d $chunkdir ]] && echo "$chunkdir not found, exiting" && exit 1
-[[ -d $outmain ]] && echo "$outmain" must be empty folder && exit 1
+# [[ -d $outmain ]] && echo "$outmain" must be empty folder && exit 1
 [[ ! -d $outmain ]] && mkdir $outmain
 
 n=0
@@ -19,6 +19,7 @@ maxjobs=15
 
 for genepath in `ls -d $datlongdir/*.Robj`; do
 	Rscript $runscript $genepath $chunkdir $outmain&
+	echo "Rscript $runscript $genepath $chunkdir $outmain"
 	# limit jobs
 	if (( $(($((++n)) % $maxjobs)) == 0 )) ; then
 		wait # wait until all have finished (not optimal, but most times good enough)
