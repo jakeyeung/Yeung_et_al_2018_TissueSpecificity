c6b88152a49619e0c54af6d556d42e21c27a0b10
Jake Yeung <jake.yeung@epfl.ch>
2015-11-26 15:57:21 +0100

    RemoveP2 names to make things prettier

--- scripts/mara/analyze_mara.complex_svd.R
+++ scripts/mara/analyze_mara.complex_svd.R
@@ -10,6 +10,7 @@ source("scripts/functions/PlotFunctions.R")
 source("scripts/functions/SvdFunctions.R")
 source("scripts/functions/PlotActivitiesFunctions.R")
 source("scripts/functions/LoadActivitiesLong.R")
+source("scripts/functions/RemoveP2Name.R")
 
 library(dplyr)  # problems with plyr old package
 library(reshape2)
@@ -27,12 +28,15 @@ indir <- "/home/yeung/projects/tissue-specificity/results/MARA/high.entropy.gene
 # indir <- "/home/yeung/projects/tissue-specificity/results/MARA/MARA_motevo_dhs_multigene.dist_filt.vitalit/expressed_genes_deseq_int.centeredTRUE.5000"
 # indir <- "/home/yeung/projects/tissue-specificity/results/MARA/MARA_motevo_dhs_multigene.dist_filt.50000/expressed_genes_deseq_int.centeredTRUE"
 # indir <- "/home/yeung/projects/tissue-specificity/results/MARA/expressed_genes_deseq_int"
-indir <- "/home/yeung/projects/tissue-specificity/results/MARA/bic_modules/Liver.centeredTRUE"
+indir <- "/home/yeung/projects/tissue-specificity/results/MARA/MARA_motevo_with_se.redo/activities"
+# indir <- "/home/yeung/projects/tissue-specificity/results/MARA/bic_modules/Liver.centeredTRUE"
+# indir <- "/home/yeung/projects/tissue-specificity/results/MARA/bic_modules/TissueWide.centeredTRUE"
 
 act.long <- LoadActivitiesLong(indir)
 
 # PlotActivitiesWithSE(subset(act.long, gene == "NR5A1.2.p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "RORA.p2"))
+# PlotActivitiesWithSE(subset(act.long, gene == "HIC1.p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "MEF2.A.B.C.D..p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "NFIL3.p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "bHLH_family.p2"))
@@ -45,7 +49,7 @@ act.long <- LoadActivitiesLong(indir)
 # # PlotActivitiesWithSE(subset(act.long, gene == "HNF1A.p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "REST.p3"))
 # PlotActivitiesWithSE(subset(act.long, gene == "MEF2.A.B.C.D..p2" & tissue %in% c("Mus", "BFAT")))
-PlotActivitiesWithSE(subset(act.long, gene == "HNF4A_NR2F1.2.p2"))
+
 # PlotActivitiesWithSE(subset(act.long, gene == "RXRG_dimer.p3"))
 # PlotActivitiesWithSE(subset(act.long, gene == "RUNX1..3.p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "SRF.p3"))
@@ -58,8 +62,19 @@ PlotActivitiesWithSE(subset(act.long, gene == "HNF4A_NR2F1.2.p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "ELK1.4_GABP.A.B1..p3"))
 # PlotActivitiesWithSE(subset(act.long, gene == "PAX2.p2"))
 # PlotActivitiesWithSE(subset(act.long, gene == "FOXA2.p3"))
-# PlotActivitiesWithSE(subset(act.long, gene == "ONECUT1.2.p2"))
-PlotActivitiesWithSE(subset(act.long, gene == "SNAI1..3.p2"))
+PlotActivitiesWithSE(subset(act.long, gene == "ONECUT1.2.p2"))
+PlotActivitiesWithSE(subset(act.long, gene == "CUX2.p2"))
+# PlotActivitiesWithSE(subset(act.long, gene == "SNAI1..3.p2"))
+PlotActivitiesWithSE(subset(act.long, gene == "RXRG_dimer.p3"))
+PlotActivitiesWithSE(subset(act.long, gene == "RORA.p2"))
+
+# PlotActivitiesWithSE(subset(act.long, gene == "EWSR1.FLI1.p2"))
+# PlotActivitiesWithSE(subset(act.long, gene == "MEF2.A.B.C.D..p2"))
+
+PlotActivitiesWithSE.rnaseq(subset(act.long, gene == "HNF4A_NR2F1.2.p2" & experiment == "rnaseq"), jtitle = "HNF4A_NR2F1/2 motif activity") + theme_bw(24)
+PlotActivitiesWithSE.rnaseq(subset(act.long, gene == "HNF1A.p2" & experiment == "rnaseq"), jtitle = "HNF1A motif activity") + theme_bw(24)
+PlotActivitiesWithSE.rnaseq(subset(act.long, gene == "MEF2.A.B.C.D..p2" & experiment == "rnaseq"), jtitle = "MEF2A|B|C|D motif activity") + theme_bw(24)
+
 
 # To complex --------------------------------------------------------------
 
@@ -68,6 +83,8 @@ act.complex <- TemporalToFrequencyDatLong(act.long, period = 24, n = 8, interval
 act.complex$exprs.adj <- act.complex$exprs.transformed * act.complex$frac.weight
 act.complex$mod.exprs <- Mod(act.complex$exprs.transformed)
 act.complex$mod.exprs.adj <- Mod(act.complex$exprs.adj)
+
+act.complex$gene <- sapply(as.character(act.complex$gene), RemoveP2Name)
 
 # act.complex <- subset(act.complex, !tissue %in% c("Liver"))
 
@@ -93,7 +110,7 @@ s.act <- SvdOnComplex(act.complex, value.var = "exprs.adj")
 
 jlayout <- matrix(c(1, 2), 1, 2, byrow = TRUE)
 for (comp in seq(1)){
-  eigens.act <- GetEigens(s.act, period = 24, comp = comp, adj.mag = TRUE)
+  eigens.act <- GetEigens(s.act, period = 24, comp = comp, adj.mag = TRUE, constant.amp = 4, label.n = 14)
   multiplot(eigens.act$v.plot, eigens.act$u.plot, layout = jlayout)
 }
 
