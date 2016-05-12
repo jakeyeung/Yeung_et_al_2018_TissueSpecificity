2ca307204d18b3c9223df30c8454bd52cef9bf1c
Jake Yeung <jake.yeung@epfl.ch>
2016-05-12 16:10:34 +0200

    Plot tissue specific modules summary

--- scripts/functions/NcondsAnalysisFunctions.R
+++ scripts/functions/NcondsAnalysisFunctions.R
@@ -1,3 +1,64 @@
+CountModels <- function(fits.best.sub){
+  fits.sum <- fits.best.sub %>%
+    group_by(model) %>%
+    summarise(count = length(model))
+  fits.sum <- fits.sum[order(fits.sum$count, decreasing = TRUE), ]
+  fits.best.sub$model <- factor(as.character(fits.best.sub$model), levels = fits.sum$model)
+  fits.sum <- OrderDecreasing(fits.sum, jfactor = "model", jval = "count")
+  return(fits.sum)
+}
+
+PlotPolarHistogram <- function(fits.sub, countstring="Count"){
+  # countstring="Count" or "NormCount"
+  fits.sub.sum <- CountModels(fits.sub)
+  # bin manually
+  # 24 and 0 are the same bin
+  fits.sub$bin <- sapply(fits.sub$phase.avg, function(p){
+    if (p > 0.5){
+      phase.bin <- round(p, digits = 0)
+    } else {
+      phase.bin <- 24
+    }
+    return(phase.bin)
+  })
+  fits.sub.forhist <- fits.sub %>%
+    group_by(model, bin) %>%
+    summarise(Count = length(bin))
+  fits.sub.forhist$bin <- fits.sub.forhist$bin - 0.5
+  
+  # add label to indicate number of genes
+  n.genes.hash <- hash(as.character(fits.sub.sum$model), fits.sub.sum$count)
+  # rename tissues
+  fits.sub.forhist$model.n <- as.factor(sapply(as.character(fits.sub.forhist$model), function(m) paste0(m, " N=", n.genes.hash[[m]])))
+  tiss.ordered <- sapply(as.character(fits.sub.sum$model), function(m) paste0(m, " N=", n.genes.hash[[m]]))
+  fits.sub.forhist$model.n <- factor(as.character(fits.sub.forhist$model.n), levels = tiss.ordered)
+  # keep original labels
+  fits.sub.forhist$tissue <- sapply(as.character(fits.sub.forhist$model.n), function(jlab){
+    strsplit(jlab, " ")[[1]][[1]]
+  })
+  fits.sub.forhist$tissue <- factor(as.character(fits.sub.forhist$tissue), levels = names(tiss.ordered))
+  fits.sub.forhist <- fits.sub.forhist %>%
+    group_by(model) %>%
+    mutate(NormCount = Count / sum(Count))
+  
+  # normalized by sum
+  m2 <- ggplot(fits.sub.forhist, aes_string(x = "bin", y = countstring)) +
+    geom_bar(stat = "identity", width = 1) + 
+    facet_wrap(~tissue) + 
+    scale_x_continuous(limits = c(0, 24), breaks = seq(2, 24, 2)) +
+    expand_limits(y = 0) +
+    expand_limits(x = c(0, 24)) +
+    xlab("Phase (h)") + 
+    ylab("Count") +
+    theme_bw() + 
+    theme(panel.grid.major = element_line(size = 0.5, colour = "grey"), panel.grid.minor = element_blank(), 
+          panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.position="bottom",
+          axis.title = element_text(size = 24),
+          strip.text = element_text(size = 12)) + 
+    coord_polar(theta = "x")
+  return(m2)
+}
+
 FilterKidneyLiverGenes <- function(param.vec, amp.min){
   # return TRUE if Kidney and Liver amplitudes are above amp.min
   liv.amp <- 0
