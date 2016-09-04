41e0ed060023ca6ffc9f92e58778273af886ef66
Jake Yeung <jake.yeung@epfl.ch>
2016-09-04 09:01:03 +0200

    PlotComplex for lambda

--- scripts/liver_kidney_WTKO/downstream.mara_analysis.global_lambda.R
+++ scripts/liver_kidney_WTKO/downstream.mara_analysis.global_lambda.R
@@ -5,6 +5,72 @@
 
 rm(list=ls())
 
+library(dplyr)
+library(ggplot2)
+library(hash)
+
+source("scripts/functions/ListFunctions.R")
+source("scripts/functions/LiverKidneyFunctions.R")
+source("scripts/functions/PlotGeneAcrossTissues.R")
+source("scripts/functions/NcondsFunctions.R")
+source("scripts/functions/SvdFunctions.R")
+source("scripts/functions/GetClockGenes.R")
+source("scripts/functions/BiomartFunctions.R")
+source("scripts/functions/PlotActivitiesFunctions.R")
+source("scripts/functions/GetTFs.R")
+
+
+# Functions ---------------------------------------------------------------
+
+PlotComplex.coloredbylambda <- function(dat, omega = 2 * pi / 24, 
+                           title = "My title", xlab = "Amplitude of activity", ylab = "Phase of activity (CT)", 
+                           ampscale = 2, constant.amp = 5, dot.col = "gray85", jsize = 22){
+    # Convert complex to amplitude (2 * fourier amplitude) and phase, given omega.
+    # then plot in polar coordinates
+    # fourier amplitudes are half-amplitudes of the sine-wave
+    # http://www.prosig.com/signal-processing/FourierAnalysis.pdf
+    # Default: ampscale = 2 because fourier amplitude is more like half-amplitude by default
+    # 
+    # Add colours
+    # http://stackoverflow.com/questions/20808009/remove-extra-space-and-ring-at-the-edge-of-a-polar-plot
+  
+    # dat <- data.frame(amp = Mod(vec.complex) * ampscale,
+    #                  phase = ConvertArgToPhase(Arg(vec.complex), omega = omega),
+    #                  label = labels)
+    
+    amp.max <- ceiling(max(dat$amp) * 2) / 2
+    if (amp.max <= 1){
+      amp.step <- 0.5
+    } else {
+      amp.step <- 1
+    }
+    m <- ggplot(data = dat, aes(x = amp, y = phase, label = gene, colour = lambda)) + 
+      geom_point(size = 0.5, colour = dot.col) +
+      coord_polar(theta = "y") + 
+      xlab(xlab) +
+      ylab(ylab) +
+      ggtitle(title) +
+      scale_y_continuous(limits = c(0, 24), breaks = seq(6, 24, 6)) + 
+      scale_x_continuous(limits = c(0, amp.max), breaks = seq(0, amp.max, length.out = 2)) + 
+      theme_bw(jsize) + 
+      geom_vline(xintercept = seq(0, amp.max, length.out = 2), colour = "grey50", size = 0.2, linetype = "dashed") +
+      geom_hline(yintercept = seq(6, 24, by = 6), colour = "grey50", size = 0.2, linetype = "solid") +
+      theme(panel.grid.major = element_line(size = 0.5, colour = "grey"), panel.grid.minor = element_blank(), 
+            panel.background = element_blank(), axis.line = element_line(colour = "black"),legend.position="bottom",
+            panel.border = element_blank(),
+            legend.key = element_blank(),
+            axis.ticks = element_blank(),
+            panel.grid  = element_blank())
+    
+    # add text
+    dat.txt <- subset(dat, gene != "")
+    if (constant.amp != FALSE){
+      m <- m + geom_text_repel(data = dat.txt, aes(x = amp, y = phase, label = gene), size = constant.amp)
+    } else {
+      m <- m + geom_text_repel(data = dat.txt, aes(x = amp, y = phase, size = amp, label = gene))
+    }
+    return(m)
+}
 
 # Load --------------------------------------------------------------------
 
@@ -14,7 +80,7 @@ outdirs <- list.dirs(outmain.lambda, recursive = FALSE, full.names = TRUE)
 outdir <- file.path(outmain.lambda, "promoters.Liver_SV129,Kidney_SV129.Liver_BmalKO,Kidney_BmalKO-Liver_SV129,Kidney_SV129.g=1001.lambda.0.0479929")
 omega <- 2 * pi / 24
 
-lapply(outdirs, function(outdir){
+act.complex <- lapply(outdirs, function(outdir){
   indir <-  file.path(outdir, "atger_with_kidney.bugfixed")
   source("scripts/functions/LoadActivitiesLong.R")
   act.long <- LoadActivitiesLongKidneyLiver(indir, collapse.geno.tissue = TRUE, shorten.motif.name = TRUE)
@@ -30,11 +96,15 @@ lapply(outdirs, function(outdir){
   act.complex$lambda <- lambda  
   return(act.complex)
 })
+act.complex <- do.call(rbind, act.complex)
 
-s.act <- SvdOnComplex(act.complex, value.var = "exprs.transformed")
+
+# Show how HIC1 amplitude changes over time -------------------------------
+
+jsub <- subset(act.complex, tissue == "Liver_SV129" & gene %in% c("HIC1", "bHLH_family", "RORA", "NFIL3"))
 
 
-max.labs <- 20
-jtitle <- ""
-eigens.act <- GetEigens(s.act, period = 24, comp = 1, adj.mag = TRUE, constant.amp = 4, label.n = max.labs, jtitle = jtitle, peak.to.trough = TRUE)
-print(eigens.act$u.plot + ggtitle(jmod))
+ggplot(jsub, aes(x = lambda, y = amp)) + geom_point()  + facet_wrap(~gene)
+
+PlotComplex.coloredbylambda(jsub, xlab = "Log2 Fold Change", ylab = "ZT", title = "Global lambda", jsize = 16, constant.amp = 4)
+
