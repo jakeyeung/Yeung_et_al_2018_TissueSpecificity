ff4ee8afbc362695f9613023370398d996041129
Jake Yeung <jake.yeung@epfl.ch>
2016-11-05 22:47:58 +0100

    init: plot hits onto map without colour

--- scripts/gene_enrichment_analysis/go_terms_plot_tree.over_time.R
+++ scripts/gene_enrichment_analysis/go_terms_plot_tree.over_time.R
@@ -3,7 +3,7 @@
 
 library("Rgraphviz")
 library("GOFunction")
-library(org.Hs.eg.db)
+# library(org.Hs.eg.db)
 library(org.Mm.eg.db)
 library(GO.db)
 library(graph)
@@ -51,86 +51,38 @@ enrichment.sum$jlab <- apply(enrichment.sum, 1, function(row) ifelse(row[3] > 1,
 
 print(PlotAmpPhase(subset(enrichment.sum, amp > 1.25), lab_str = "jlab", textsize = 5) + xlim(c(0, 2)))
 
-# Query DBs ---------------------------------------------------------------
 
-.sql <- paste("select distinct go_id goid,term name from go_term where ontology='",
-              toupper(ontology), "'", sep="")
-allTermName <-  dbGetQuery(conn,.sql)
+# Visualize top 25 GO terms in tree ---------------------------------------
 
-# Get relevant genes ------------------------------------------------------
+top.terms <- head(subset(data.frame(enrichment.sum %>% arrange(desc(amp))), select = c(GO.ID, Term, amp, phase, pval)), n = 25)$GO.ID
 
-load("Robjs/liver_kidney_atger_nestle/fits.long.multimethod.filtbest.staggeredtimepts.bugfixed.annotated.Robj", v=T)
-jmethod <- "g=1001"
-jmod <- "Liver_SV129,Liver_BmalKO"
-fits.long.filt <- subset(fits.long.filt, method == jmethod)
-fg.genes <- as.character(subset(fits.long.filt, model == jmod)$gene)
-bg.genes <- as.character(subset(fits.long.filt)$gene)
-sym2entrez <- CreateSym2Entrez()
-fg.genes.entrez <- ConvertSym2Entrez(fg.genes, sym2entrez)
-bg.genes.entrez <- ConvertSym2Entrez(bg.genes, sym2entrez)
-
-data(exampledata)
-
-jorgo <- "org.Mm.eg.db"
-sigTerm <- GOFunction(fg.genes.entrez, bg.genes.entrez, organism=jorgo, ontology="BP", fdrmethod="BY", fdrth=0.05, ppth=0.05, pcth=0.05, poth=0.05, peth=0.05, bmpSize=2000, filename="sigTerm")
-
-organism=jorgo; ontology="BP"; fdrmethod="BY"; 
-fdrth=0.05; ppth=0.05; pcth=0.05; poth=0.05; 
-peth=0.0; bmpSize=2000; filename="sigTerm"
-
-
-# Hack code to get graph visualization without output to file -------------
-
-## Extracting the gene annotation data
-.sql <-  paste("select distinct t1.gene_id,t2.go_id",
-               " from genes as t1 inner join",paste("go", tolower(ontology), "all", sep = "_"),
-               " as t2 on t1._id=t2._id",seq="")
-organism <- strsplit(organism,".db")
-organism <- organism[[1]]
-conn <- get(paste(organism, "_dbconn", sep = ""))()
-generalAnn <- dbGetQuery(conn, .sql)
-annRef <- generalAnn[generalAnn[,1] %in% bg.genes.entrez,]
-annInterest <- generalAnn[generalAnn[,1] %in% fg.genes.entrez,]
-
-## Calculating the statistically significant terms for fg.genes.entrez
-cat("Finding statistically significant terms...\n")
-termInfo <- enrichmentFunction(annRef, annInterest,fdrmethod,fdrth)
-sigTerm <- termInfo$sigTerm
-if(nrow(sigTerm)==0){
-  warning("There is no significant term! \n")
-  return(NULL)
-}
-allTerm <- termInfo$allTerm
-
-## Loading the GO structure data
-conn <- get("GO_dbconn")()
+ontology <- "BP"
 
 ## Finding the GO term name for statistically siginificant terms
+conn <- get("GO_dbconn")()
 .sql <- paste("select distinct go_id goid,term name from go_term where ontology='",
               toupper(ontology), "'", sep="")
 allTermName <-  dbGetQuery(conn,.sql)
-sigTermName <- allTermName[allTermName[,1] %in% sigTerm[,1],]
+sigTermName <- allTermName[allTermName[,1] %in% top.terms,]
 sigTermName <- sigTermName[order(sigTermName[,1]),]
-sigTerm <- sigTerm[order(sigTerm[,1]),]
+sigTerm <- list()
 sigTerm$name <- sigTermName[,2]
-sigTerm <- sigTerm[,c(1,6,2,3,4,5)]
 
 ## Finding the relationship between statistically significant terms
 .sql <- paste("select distinct t1.go_id parentid,t2.go_id childid from ", paste("go", tolower(ontology),   "offspring", sep = "_"), " as t3 inner join  go_term as t1 on t1._id=t3._id inner join go_term as t2", " on t2._id=t3._offspring_id", sep="")
 allTermRelation <-  dbGetQuery(conn,.sql)
-sigTermRelation <- allTermRelation[(allTermRelation[,1] %in% sigTerm[,1]) & (allTermRelation[,2] %in% sigTerm[,1]),]
+sigTermRelation <- allTermRelation[(allTermRelation[,1] %in% top.terms) & (allTermRelation[,2] %in% top.terms),]
 rm(allTermRelation)
 
 ## Finding the offspring terms for each statistically significant terms
 
 ## Local redundance
-cat("Treating for local redundant terms...\n")
-sigTerm_LocalRedun <- localRedundancy(sigTerm, generalAnn, sigTermRelation, annRef, annInterest, ppth, pcth)
+# cat("Treating for local redundant terms...\n")
+# sigTerm_LocalRedun <- localRedundancy(sigTerm, generalAnn, sigTermRelation, annRef, annInterest, ppth, pcth)
 
 ##Global redundance
-
-cat("Treating for global redundant terms...\n")
-sigTerm_GlobalRedun <- globalRedundancy(generalAnn, sigTermRelation, annRef, annInterest, sigTerm_LocalRedun, poth,  peth)
+# cat("Treating for global redundant terms...\n")
+# sigTerm_GlobalRedun <- globalRedundancy(generalAnn, sigTermRelation, annRef, annInterest, sigTerm_LocalRedun, poth,  peth)
 
 
 ## Display the GO DAG plot for the significant terms
@@ -139,18 +91,15 @@ cat("Visualizing the GO DAG...\n")
 
 # take top 10 hits 
 pvalcutoff <- 1
-top.go.terms <- as.character(subset(sigTerm, pvalue < pvalcutoff)$goid)
-sigDAG <- createGODAG(top.go.terms, ontology)
-allDAGTerm <- allTerm[allTerm[,1] %in% nodes(sigDAG),]
+sigDAG <- createGODAG(top.terms, ontology)
+allDAGTerm <- allTermName$goid[allTermName$goid %in% nodes(sigDAG)]
 
-dagTermName <- allTermName[allTermName[,1] %in% allDAGTerm[,1],]
-dagTermName <- dagTermName[order(dagTermName[,1]),]
-allDAGTerm <- allDAGTerm[order(allDAGTerm[,1]),]
-allDAGTerm$name <- dagTermName[,2]
-allDAGTerm <- allDAGTerm[,c(1,6,2,3,4,5)]
-sigTermID <- as.character(sigTerm[,1])
-sigTerm_LocalRedunID <- as.character(sigTerm_LocalRedun[,1])
-sigTerm_GlobalRedunID <- as.character(sigTerm_GlobalRedun[,1])
+dagTermName <- allTermName$goid[allTermName$goid %in% allDAGTerm]
+dagTermName <- dagTermName[order(dagTermName)]
+allDAGTerm <- allDAGTerm[order(allDAGTerm)]
+allDAGTerm.name <- allTermName$name[allTermName$goid %in% allDAGTerm]
+# allDAGTerm <- allDAGTerm[,c(1,6,2,3,4,5)]
+sigTermID <- as.character(top.terms)
 
 # BEGIN PLOTTING
 
@@ -162,31 +111,9 @@ graphAttrs$node$fontsize <- '20'
 nodeAttrs <- list()
 edgeAttrs <- list()
 
-allTerm <- as.character(allDAGTerm[,1])
-GO.names <- as.character(allDAGTerm$name)
+allTerm <- as.character(allDAGTerm)
+GO.names <- as.character(allDAGTerm.name)
 nodeAttrs$label[allTerm] <- allTerm
-
-rmLocalTerm <- setdiff(sigTermID, sigTerm_LocalRedunID)
-nodeAttrs$color[rmLocalTerm] <- rep('red', length(rmLocalTerm))
-nodeAttrs$shape[rmLocalTerm] <- rep('circle', length(rmLocalTerm))
-
-rmGlobalTerm <- setdiff(sigTerm_LocalRedunID, sigTerm_GlobalRedunID)
-nodeAttrs$color[rmGlobalTerm] <- rep('red', length(rmGlobalTerm))
-nodeAttrs$shape[rmGlobalTerm] <- rep('box', length(rmGlobalTerm))
-nodeAttrs$height[rmGlobalTerm] <- rep('0.7', length(rmGlobalTerm))
-nodeAttrs$width[rmGlobalTerm] <- rep('0.7', length(rmGlobalTerm))
-
-nodeAttrs$color[sigTerm_GlobalRedunID] <- rep('red', length(sigTerm_GlobalRedunID))
-nodeAttrs$shape[sigTerm_GlobalRedunID] <- rep('rectangle', length(sigTerm_GlobalRedunID))
-nodeAttrs$height[sigTerm_GlobalRedunID] <- rep('0.7', length(sigTerm_GlobalRedunID))
-nodeAttrs$width[sigTerm_GlobalRedunID] <- rep('1.1', length(sigTerm_GlobalRedunID))
-
-allDAGTerm[allDAGTerm[,5]<2.2E-16,5] <- 2.2E-16;
-allDAGTerm[allDAGTerm[,6]<2.2E-16,6] <- 2.2E-16;
-allDAGTerm$colorran <- round(log10(allDAGTerm[,6])-range(log10(allDAGTerm[,6]))[1] + 1)
-mm <- max(allDAGTerm$colorran)
-colorMap <- heat.colors(mm)
-nodeAttrs$fillcolor[allTerm] <- unlist(lapply(allDAGTerm$colorran, function(x) return(colorMap[x])))
 
 weightsList <- edgeWeights(sigDAG)
 to <- lapply(weightsList, names)
@@ -202,8 +129,4 @@ nodeAttrs$label[allTerm] <- GO.names
 nodeAttrs$fixedsize[allTerm] <- TRUE
 nodeAttrs$fontsize[allTerm] <- 200
 # nodeAttrs$cex[allTerm] <- 50
-plot(sigDAG, attrs = graphAttrs, nodeAttrs = nodeAttrs, edgeAttrs = edgeAttrs, main = "test2")
-
-
-
-
+plot(sigDAG, attrs = graphAttrs, nodeAttrs = nodeAttrs, edgeAttrs = edgeAttrs, main = "Testing")
