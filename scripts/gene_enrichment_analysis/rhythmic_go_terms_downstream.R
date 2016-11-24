0a34434ad772da3af54ccbd7f9143b1af84c015b
Jake Yeung <jake.yeung@epfl.ch>
2016-11-24 09:38:05 +0100

    Play with downstream of GO terms.

--- scripts/gene_enrichment_analysis/rhythmic_go_terms_downstream.R
+++ scripts/gene_enrichment_analysis/rhythmic_go_terms_downstream.R
@@ -22,10 +22,15 @@ source("scripts/functions/PlotFunctions.R")
 source("scripts/functions/HashFunctions.R")
 source("scripts/functions/PhaseColorFunctions.R")
 
+manual.terms <- TRUE
+
 # Load rhythmic GO terms --------------------------------------------------
 
+jmod <- "Liver_BmalKO"
+jmod <- "Liver_SV129"
 jmod <- "Kidney_SV129"
 indir <- "/home/yeung/projects/tissue-specificity/Robjs/GO_analysis"
+jmod <- "Liver_SV129,Liver_BmalKO"
 fname <- paste0("model", jmod, ".all.Robj")
 inf <- file.path(file.path(indir, fname))
 
@@ -38,27 +43,70 @@ enrichment.sum <- enrichment %>%
   arrange(desc(amp))
 
 # show top hits 
-top.n <- 50
+top.n <- 30
 print(subset(data.frame(head(enrichment.sum, n = top.n)), select = c(GO.ID, Term, amp, phase, pval)))
 
-# plot top hits
-jGOID <- "GO:0090315"
-jGOID <- "GO:0000001"
+# # plot top hits
+# jGOID <- "GO:0090315"
+# jGOID <- "GO:0000001"
+jGOID <- "GO:0008643"
+jGOID <- "GO:0044255"
+jGOID <- "GO:0044262"
+jGOID <- "GO:0015695"
+jGOID <- "GO:0072337"
+# search for a term
+enrichment[grepl("cellular copper ion homeostasis", enrichment$Term), ]
+enrichment[grepl("copper ion homeostasis", enrichment$Term), ]
+enrichment[grepl("potassium ion transmembrane transport", enrichment$Term), ]
+enrichment[grepl("cation transport", enrichment$Term), ]
+jGOID <- "GO:0042254"  # ribi
+jGOID <- "GO:0006487"  # N-glycosylation
+jGOID <- "GO:0046326"  # glucose import
+jGOID <- "GO:0006813"  # potassium ion transport
+jGOID <- "GO:0043266"  # reg of pot ion transport
+jGOID <- "GO:0044070"  # anion transport
+jGOID <- "GO:0006878"  # cellular copper homeostasis
+jGOID <- "GO:0055070"  # copper ion homeostasis 
 jtest <- subset(enrichment, GO.ID == jGOID)
 jname <- jtest$Term
-ggplot(jtest, aes(x = tstart + 3, y = minuslogpval)) + geom_point() + geom_line() + ggtitle(jname)
+ggplot(jtest, aes(x = tstart + 3, y = minuslogpval)) + geom_point() + geom_line() + ggtitle(jname) + ylab("Minus Log10 Pval") + xlab("ZT") + theme_bw()
+
 
 
 # Get DAG graph -----------------------------------------------------------
 
-top.terms <- head(subset(data.frame(enrichment.sum %>% arrange(desc(amp))), select = c(GO.ID, Term, amp, phase, pval)), n = top.n)$GO.ID
-top.terms <- c("GO:0006812", "GO:0006865")
+if (!manual.terms){
+  top.terms <- head(subset(data.frame(enrichment.sum %>% arrange(desc(amp))), select = c(GO.ID, Term, amp, phase, pval)), n = top.n)$GO.ID
+  jfontsize <- 150
+  jamp.cutoff <- 0.7
+} else {
+  if (jmod == "Liver_SV129"){
+    jterms <- enrichment.sum[grepl("cellular carbohydrate metabolic|cellular lipid metabolic", enrichment.sum$Term), ]$GO.ID
+    # top.terms <- c("GO:0044255", "GO:0044262")
+    top.terms <- jterms
+    jfontsize <- 50
+    jamp.cutoff <- 0.5
+  } else if (jmod == "Kidney_SV129"){
+    jterms <- enrichment.sum[grepl("cation transport|amino acid transport", enrichment.sum$Term), ]$GO.ID
+    jterms <- enrichment.sum[grepl("cation transport|amino acid transport|potassium ion transmembrane transport|cellular copper ion homeostasis", enrichment.sum$Term), ]$GO.ID
+    top.terms <- jterms
+    jfontsize <- 50
+    jamp.cutoff <- 0.3
+  } else if (jmod == "Liver_SV129,Liver_BmalKO"){
+    jterms <- enrichment.sum[grepl("ribosome biogenesis|DNA replication initiation|response to insulin|regulation of fibrinolysis|positive regulation of glucose import", enrichment.sum$Term), ]$GO.ID
+    top.terms <- jterms
+    jamp.cutoff <- 0.7
+    jfontsize <- 100
+  } else if (jmod == "Liver_BmalKO"){
+    top.terms <- c()
+  }
+}
 
 ontology <- "BP"
 sigDAG <- createGODAG(top.terms, ontology)
 
 # Plot DAG graph ----------------------------------------------------------
 
-pdf(paste0("plots/GO_analysis/GO_tree.", jmod, ".pdf"))
-PlotDAGGraph(sigDAG, enrichment.sum, jfontsize = 75, jtitle = jmod, amp.cutoff = 0.3)
+pdf(paste0("plots/GO_analysis/GO_tree.", jmod, ".manualterms2.", manual.terms, ".pdf"))
+PlotDAGGraph(sigDAG, enrichment.sum, jfontsize = jfontsize, jtitle = jmod, amp.cutoff = jamp.cutoff)
 dev.off()
